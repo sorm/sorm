3460b055b0869f178969bcdfca54e821c7825399
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-05-02 16:24:34 +0400

    nice updates

--- src/main/scala/vorm/reflection/Reflection.scala
+++ src/main/scala/vorm/reflection/Reflection.scala
@@ -5,7 +5,7 @@ import reflect.mirror
 object Reflection {
 
   private val tpeCache = collection.mutable.Map[mirror.Type, Type]()
-  def tpe(mt: mirror.Type): Type =
+  def tpe[T](mt: mirror.Type): Type =
     try tpeCache(mt)
     catch {
       case _ => {
@@ -14,23 +14,33 @@ object Reflection {
         t
       }
     }
-
+  def tpe[T: TypeTag]: Type =
+    tpe(tag[T].tpe)
+  def tpe[T: TypeTag](instance: T): Type =
+    tpe(tag[T].tpe)
 
   class Type(mt: mirror.Type) {
     lazy val generics =
-      mt.typeArguments.indices.map(i => new Generic(mt.typeArguments(i), this, i))
+      mt.typeArguments.indices
+        .map(i => new Generic(mt.typeArguments(i), this, i))
 
     lazy val properties =
-      mt.members.filter(m => m.owner == mt && !m.isMethod)
+      mt.members.filter(m => !m.isMethod)
         .map(p => new Property(p.name.decoded.trim, tpe(p.typeSignature), this))
 
-    lazy val propertiesMap =
+    lazy val propertyByNameMap =
       properties.map(p => p.name -> p).toMap
+
+    override def toString = mt.toString
   }
 
-  class Generic(mt: mirror.Type, val owner: Type, val index: Int) extends Type(mt)
+  class Generic(mt: mirror.Type, val owner: Type, val index: Int) extends Type(mt) {
+    override def toString = super.toString + "@" + owner.toString
+  }
 
-  class Property(val name: String, val tpe: Type, val owner: Type)
+  class Property(val name: String, val tpe: Type, val owner: Type) {
+    override def toString = owner.toString + "." + name + ": " + tpe.toString
+  }
 
 }
 
---
+++ src/test/scala/vorm/reflection/Sandbox.scala
@@ -0,0 +1,15 @@
+package vorm.reflection
+
+import Reflection._
+
+object Sandbox extends App {
+  case class Genre(name: String)
+  case class Artist(id: String, name: String, genres: Set[Genre], tags: Set[String])
+
+  tpe[Artist].properties.foreach(println)
+
+  println(tpe(Genre("")))
+
+  tpe[Artist].propertyByNameMap("genres").tpe.generics.foreach(println)
+
+}

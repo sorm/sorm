7bbc69991d05409bccb11b64d1d4bcc247e427f6
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-05-03 01:23:14 +0400

    attempt on specs

---
+++ src/test/scala/vorm/reflection/Spec.scala
@@ -0,0 +1,13 @@
+package vorm.reflection
+
+//import org.scalatest.matchers.ShouldMatchers
+//import collection.mutable.Stack
+//import org.scalatest.{FunSuite, FunSpec}
+//
+//class Spec extends FunSuite with ShouldMatchers {
+//
+//  test("AnyVal detection") {
+////    reflection(123)
+//  }
+//
+//}

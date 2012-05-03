55dac5ea905da90d4619b1621185a2e85bc4a898
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-05-03 23:54:06 +0400

    tests moved to spec

--- src/test/scala/vorm/extensions/Sandbox.scala
+++ src/test/scala/vorm/extensions/Sandbox.scala
@@ -2,17 +2,4 @@ package vorm.extensions
 
 object Sandbox extends App {
 
-  assert(342.isInstanceOf1[AnyVal])
-  assert(!Seq(123, 2313).isInstanceOf1[AnyVal])
-  assert(Seq(123, 2313).isInstanceOf1[Seq[_]])
-
-  assert(Seq(123, 2313).isInstanceOf1[Traversable[_]])
-  assert(Map("a" -> 1, "b" -> 0).isInstanceOf1[Traversable[_]])
-
-  assert(Seq(123, 2313).isInstanceOf1[Seq[Int]])
-  assert(!Seq(123, 2313).isInstanceOf1[Seq[String]])
-
-  assert(Map("a" -> 1, "b" -> 0).isInstanceOf1[Map[_, _]])
-  assert(!Map("a" -> 1, "b" -> 0).isInstanceOf1[AnyVal])
-
 }

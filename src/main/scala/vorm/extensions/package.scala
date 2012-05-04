37667f1267f7b2f1859c3ce55c44cf7304c5405d
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-05-05 02:04:38 +0400

    AnyWrapper moved out

--- src/main/scala/vorm/extensions/package.scala
+++ src/main/scala/vorm/extensions/package.scala
@@ -1,38 +1,8 @@
 package vorm
 
+
 package object extensions {
 
   implicit def anyWrapper[T: TypeTag](x: T) = new AnyWrapper(x)
-  class AnyWrapper[T: TypeTag](x: T) {
-    def as[Result](closure: T => Result) =
-      closure(x)
 
-    def isInstanceOf1[T2: TypeTag] =
-      tag[T].tpe <:< tag[T2].tpe
-
-    def isEmpty = {
-      x match {
-        case null | () => true
-        case x: Boolean => !x
-        case x: Byte => x == 0.toByte
-        case x: Short => x == 0.toShort
-        case x: Char => x == 0.toChar
-        case x: Int => x == 0
-        case x: Long => x == 0l
-        case x: Float => x == 0f
-        case x: Double => x == 0d
-        case _ => false
-      }
-    }
-
-    def asNonEmpty =
-      if (isEmpty) None else Some(x)
-
-    def asSatisfying(p: T => Boolean) =
-      if (p(x)) Some(x) else None
-
-    def println() {
-      Console.println(x)
-    }
-  }
 }
--- src/test/scala/vorm/extensions/Sandbox.scala
+++ src/test/scala/vorm/extensions/Sandbox.scala
@@ -2,4 +2,8 @@ package vorm.extensions
 
 object Sandbox extends App {
 
+
+  21.asSatisfying(_ > 20).println()
+
+
 }

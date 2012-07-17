28419ac61e5ab82f3962e1b111ee5acd9aa0eedf
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-18 03:30:01 +0400

    utils

--- src/main/scala/vorm/mapping/package.scala
+++ src/main/scala/vorm/mapping/package.scala
@@ -2,15 +2,44 @@ package vorm
 
 package object mapping {
 
+  import reflection._
 
-  implicit class StringDdlFormattedSupport
+
+  implicit class StringDdlNameSupport
     ( val string : String )
     {
       import com.google.common.base.CaseFormat._
-      def ddlFormatted
+      def ddlName
         : String
-        = UPPER_CAMEL.to(LOWER_UNDERSCORE, string)
+        = UPPER_CAMEL.to( LOWER_UNDERSCORE, string )
     }
 
+  implicit class ReflectionDdlColumnTypeSupport
+    ( val reflection : Reflection )
+    {
+      def ddlColumnType
+        ( isKeyPart : Boolean )
+        = reflection match {
+            case _ if reflection inheritsFrom reflectionOf[String]
+              ⇒ if (isKeyPart) 
+                  vorm.ddl.Column.Type.VarChar
+                else 
+                  vorm.ddl.Column.Type.Text
+            case _ if reflection inheritsFrom reflectionOf[Boolean]
+              ⇒ vorm.ddl.Column.Type.Boolean
+            case _ if reflection inheritsFrom reflectionOf[Short]
+              ⇒ vorm.ddl.Column.Type.SmallInt
+            case _ if reflection inheritsFrom reflectionOf[Int]
+              ⇒ vorm.ddl.Column.Type.Integer
+            case _ if reflection inheritsFrom reflectionOf[Long]
+              ⇒ vorm.ddl.Column.Type.BigInt
+            case _ if reflection inheritsFrom reflectionOf[Float]
+              ⇒ vorm.ddl.Column.Type.Float
+            case _ if reflection inheritsFrom reflectionOf[Double]
+              ⇒ vorm.ddl.Column.Type.Double
+            case _ if reflection inheritsFrom reflectionOf[BigDecimal]
+              ⇒ vorm.ddl.Column.Type.Decimal
+          }
+    }
 
 }

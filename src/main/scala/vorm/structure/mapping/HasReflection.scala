07eeab20b723e51e6c828d3af7990c1858353ee0
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-19 17:16:33 +0400

    traits extracted

---
+++ src/main/scala/vorm/structure/mapping/HasReflection.scala
@@ -0,0 +1,9 @@
+package vorm.structure.mapping
+
+import vorm._
+import structure._
+import reflection._
+
+trait HasReflection {
+  def reflection : Reflection
+}
--- src/main/scala/vorm/structure/mapping/package.scala
+++ src/main/scala/vorm/structure/mapping/package.scala
@@ -6,22 +6,6 @@ import reflection._
 
 package object mapping {
 
-  trait HasParent {
-    def parent : Mapping
-  }
-
-  trait HasChildren {
-    def children : collection.Seq[Mapping]
-  }
-
-  trait HasChild {
-    def child : Mapping
-  }
-
-  trait HasReflection {
-    def reflection : Reflection
-  }
-
   type Settings = collection.Map[Reflection, EntitySettings]
 
 }

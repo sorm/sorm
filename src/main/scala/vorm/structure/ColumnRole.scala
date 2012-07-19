6e05ecfe17ac4208dd2f8f1bb7310c069873b685
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-19 21:07:36 +0400

    update to nodal mapping

--- src/main/scala/vorm/structure/ColumnRole.scala
+++ src/main/scala/vorm/structure/ColumnRole.scala
@@ -5,54 +5,25 @@ sealed trait ColumnRole {
 }
 object ColumnRole {
 
-  //  Entity columns:
   sealed case class GeneratedId
     ( mapping : Mapping )
     extends ColumnRole
 
-  //  Value:
   sealed case class Value
     ( mapping : Mapping )
     extends ColumnRole
 
-  //  Tuple:
-  sealed case class TupleItemKeyPart
-    ( index : Int,
-      tupleItemIndex : Int,
-      mapping : Mapping )
-    extends ColumnRole
-
-  //  Option, Seq, Set:
-  sealed case class ItemKeyPart
+  sealed case class ForeignKeyPart
     ( index : Int,
       mapping : Mapping )
     extends ColumnRole
 
-  //  for all collections
-  sealed case class ParentKeyPart
-    ( index : Int, 
-      mapping : Mapping )
-    extends ColumnRole
-
-  //  Seq:
   sealed case class Index
     ( mapping : Mapping )
     extends ColumnRole
 
-  //  Hashed collections:
   sealed case class Hash
     ( mapping : Mapping )
     extends ColumnRole
-
-  sealed case class MapKeyKeyPart
-    ( index : Int, 
-      mapping : Mapping )
-    extends ColumnRole
-
-  sealed case class MapValueKeyPart
-    ( index : Int, 
-      mapping : Mapping )
-    extends ColumnRole
-
 
 }

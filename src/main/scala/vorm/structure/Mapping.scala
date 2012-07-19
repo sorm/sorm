4ebf28a925e653febde4414ddee8d30cfc52ff80
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-19 14:08:54 +0400

    ideas

--- src/main/scala/vorm/structure/Mapping.scala
+++ src/main/scala/vorm/structure/Mapping.scala
@@ -3,28 +3,75 @@ package vorm.structure
 import vorm.reflection._
 
 trait Mapping {
-  def membership : Option[Membership]
+  def setup : Setup
 }
 object Mapping {
-  
+  /**
+   * Root
+   */
   def apply 
-    ( reflection : Reflection,
-      setup : Setup )
-    : Mapping
-    = apply( None, reflection, setup )
-
-  def apply 
-    ( membership : Membership,
-      reflection : Reflection,
-      setup : Setup )
-    : Mapping
-    = apply( Some(membership), reflection, setup )
-
-  def apply 
-    ( membership : Option[Membership],
-      reflection : Reflection,
-      setup : Setup )
+    ( setup : Setup,
+      reflection : Reflection )
     : Mapping
     = throw new NotImplementedError
+
+  def apply 
+    ( setup : Setup,
+      reflection : Reflection,
+      parent : Mapping )
+    : Mapping
+    = throw new NotImplementedError
+
+  trait HasParent {
+    def parent : Mapping
+  }
+
+  trait HasChildren {
+    def children : Seq[Mapping]
+  }
+
+  trait HasReflection {
+    def reflection : Reflection
+  }
+
+  case class Tuple
+    ( setup : Setup,
+      reflection : Reflection,
+      parent : Mapping )
+    extends Mapping 
+    with HasParent 
+    with HasChildren
+    with HasReflection
+    {
+      lazy val children
+        = reflection.generics.view
+            .zipWithIndex.map { case (r, i) ⇒ new TupleItem(setup, r, this) }
+            .toList
+
+    }
+
+  case class TupleItem
+    ( setup : Setup,
+      reflection : Reflection,
+      parent : Mapping )
+    extends Mapping 
+    with HasParent 
+    with HasChildren
+    with HasReflection
+    {
+      lazy val children
+        = Mapping( setup, reflection, this ) ::
+          Nil
+    }
+
+
+
+  // case class Root
+  //   ( setup : Setup )
+  //   extends Mapping with HasChildren
+  //   {
+  //     lazy val children
+  //       = setup.entities.map( Mapping( setup, this) )
+  //   }
 
 }

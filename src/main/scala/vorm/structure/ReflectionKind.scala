20b84f43aaa2f29d1160a24e2ab5965b993a4458
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-19 13:26:30 +0400

    correction to accommodate reflection updates

--- src/main/scala/vorm/structure/ReflectionKind.scala
+++ src/main/scala/vorm/structure/ReflectionKind.scala
@@ -26,45 +26,45 @@ object ReflectionKind {
     ( reflection : Reflection )
     = reflection match {
         case _
-          if reflection inheritsFrom reflectionOf[collection.Seq[_]]
+          if reflection inheritsFrom Reflection[collection.Seq[_]]
           ⇒ Seq
         case _
-          if reflection inheritsFrom reflectionOf[collection.Set[_]]
+          if reflection inheritsFrom Reflection[collection.Set[_]]
           ⇒ Set
         case _
-          if reflection inheritsFrom reflectionOf[collection.Map[_, _]]
+          if reflection inheritsFrom Reflection[collection.Map[_, _]]
           ⇒ Map
         case _
-          if reflection inheritsFrom reflectionOf[scala.Option[_]]
+          if reflection inheritsFrom Reflection[scala.Option[_]]
           ⇒ Option
         case _
-          if (reflection inheritsFrom reflectionOf[AnyVal])
-          || (reflection inheritsFrom reflectionOf[String])
-          || (reflection inheritsFrom reflectionOf[org.joda.time.DateTime])
+          if (reflection inheritsFrom Reflection[AnyVal])
+          || (reflection inheritsFrom Reflection[String])
+          || (reflection inheritsFrom Reflection[org.joda.time.DateTime])
           ⇒ Value
         case _
-          if (reflection inheritsFrom reflectionOf[Tuple1[_]])
-          || (reflection inheritsFrom reflectionOf[Tuple2[_, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple3[_, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple4[_, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple5[_, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple6[_, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple7[_, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple8[_, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple9[_, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple10[_, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple11[_, _, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple12[_, _, _, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple13[_, _, _, _, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple14[_, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple15[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple16[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple17[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-          || (reflection inheritsFrom reflectionOf[Tuple18[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-//          || (reflection inheritsFrom reflectionOf[Tuple19[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-//          || (reflection inheritsFrom reflectionOf[Tuple20[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-//          || (reflection inheritsFrom reflectionOf[Tuple21[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
-//          || (reflection inheritsFrom reflectionOf[Tuple22[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+          if (reflection inheritsFrom Reflection[Tuple1[_]])
+          || (reflection inheritsFrom Reflection[Tuple2[_, _]])
+          || (reflection inheritsFrom Reflection[Tuple3[_, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple4[_, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple5[_, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple6[_, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple7[_, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple8[_, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple9[_, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple10[_, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple11[_, _, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple12[_, _, _, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple13[_, _, _, _, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple14[_, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple15[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple16[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple17[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+          || (reflection inheritsFrom Reflection[Tuple18[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+//          || (reflection inheritsFrom Reflection[Tuple19[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+//          || (reflection inheritsFrom Reflection[Tuple20[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+//          || (reflection inheritsFrom Reflection[Tuple21[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
+//          || (reflection inheritsFrom Reflection[Tuple22[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]])
           ⇒ Tuple
         case _
           ⇒ Entity
--- src/main/scala/vorm/structure/package.scala
+++ src/main/scala/vorm/structure/package.scala
@@ -20,24 +20,24 @@ package object structure {
       def ddlColumnType
         ( isKeyPart : Boolean )
         = reflection match {
-            case _ if reflection inheritsFrom reflectionOf[String]
+            case _ if reflection inheritsFrom Reflection[String]
               ⇒ if (isKeyPart) 
                   vorm.ddl.Column.Type.VarChar
                 else 
                   vorm.ddl.Column.Type.Text
-            case _ if reflection inheritsFrom reflectionOf[Boolean]
+            case _ if reflection inheritsFrom Reflection[Boolean]
               ⇒ vorm.ddl.Column.Type.Boolean
-            case _ if reflection inheritsFrom reflectionOf[Short]
+            case _ if reflection inheritsFrom Reflection[Short]
               ⇒ vorm.ddl.Column.Type.SmallInt
-            case _ if reflection inheritsFrom reflectionOf[Int]
+            case _ if reflection inheritsFrom Reflection[Int]
               ⇒ vorm.ddl.Column.Type.Integer
-            case _ if reflection inheritsFrom reflectionOf[Long]
+            case _ if reflection inheritsFrom Reflection[Long]
               ⇒ vorm.ddl.Column.Type.BigInt
-            case _ if reflection inheritsFrom reflectionOf[Float]
+            case _ if reflection inheritsFrom Reflection[Float]
               ⇒ vorm.ddl.Column.Type.Float
-            case _ if reflection inheritsFrom reflectionOf[Double]
+            case _ if reflection inheritsFrom Reflection[Double]
               ⇒ vorm.ddl.Column.Type.Double
-            case _ if reflection inheritsFrom reflectionOf[BigDecimal]
+            case _ if reflection inheritsFrom Reflection[BigDecimal]
               ⇒ vorm.ddl.Column.Type.Decimal
           }
     }

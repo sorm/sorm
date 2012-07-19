5931b56faa3bd872dfa33b941d55da268d7e31e1
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-19 20:18:59 +0400

    upd

--- src/main/scala/vorm/resultSet/package.scala
+++ src/main/scala/vorm/resultSet/package.scala
@@ -3,8 +3,41 @@ package vorm
 import vorm._
 import reflection._
 import structure._
+import jdbc._
+import ddl._
+import java.sql.ResultSet
 
 package object resultSet {
 
+  implicit class ResultSetExtensions
+    ( rs : ResultSet )
+    {
+      def results
+        [ T : TypeTag ]
+        ( columnRoles : IndexedSeq[ColumnRole] )
+        : Seq[T]
+        = {
+          def value
+            ( index : Int )
+            = columnRoles(index) match {
+                case ColumnRole.Value( mapping )
+                  ⇒ rs.value( index, mapping.column.jdbcType )
+                case ColumnRole.Hash( mapping )
+                  ⇒ rs.value( index, Column.Type.Integer.jdbcType )
+              }
 
+          val columnRolesWithIndexesStream
+            = columnRoles.toStream.zipWithIndex
+
+          val rowData
+            : Stream[(Any, ColumnRole)]
+            = columnRolesWithIndexesStream
+                .map { case (role, i) ⇒ rs.getValue(i) → role }
+
+          
+        }
+
+    }
 }
+
+

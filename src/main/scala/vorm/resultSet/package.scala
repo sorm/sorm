ecd8122d76e1b3a80d8bec8c02f8b0147d8381da
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-21 00:35:08 +0400

    upd

--- src/main/scala/vorm/resultSet/package.scala
+++ src/main/scala/vorm/resultSet/package.scala
@@ -13,45 +13,56 @@ package object resultSet {
   implicit class ResultSetExtensions
     ( rs : ResultSet )
     {
-      type MappingColumn = (structure.mapping.Table, Column)
 
       def fetchInstances
-        ( mapping : structure.mapping.Table,
-          indexes : Map[MappingColumn, Int] )
+        ( m : mapping.Table,
+          indexes : Map[(mapping.Table, Column), Int] )
         : Seq[_]
         = {
 
+          //  ideas
+          // type PrimaryKey 
+          //   = Seq[Any]
+          // type Rows 
+          //   = Map[PrimaryKey, Row]
+          // type Row 
+          //   = Map[String, Either[Any, Rows]]
+          // class Rows 
+          //   extends collection.immutable.HashMap[PrimaryKey, Map[String, Either[Any, Rows]]]
+
           case class Row
             ( data : Map[Column, Any],
-              rowsOfSubmappings : Map[structure.mapping.Table, Map[PrimaryKey, Row]] )
+              rowsOfSubTables : Map[mapping.Table, Map[PrimaryKey, Row]] )
+
           type PrimaryKey = Seq[Any]
+
             
           def fetchRowsAndClose()
             : Map[PrimaryKey, Row]
             = {
 
               def updatedRows
-                ( mapping : structure.mapping.Table,
+                ( m : mapping.Table,
                   rows : Map[PrimaryKey, Row] )
                 : Map[PrimaryKey, Row]
                 = {
                   def value
                     ( column : Column )
                     = rs.value(
-                          indexes(mapping → column),
+                          indexes(m → column),
                           column.t.jdbcType
                         )
 
                   val primaryKey
-                    = mapping.primaryKeyColumns map value
+                    = m.primaryKeyColumns map value
 
                   rows.get(primaryKey) match {
                     case Some(row)
                       ⇒ rows +
                         ( primaryKey → 
                           row.copy(
-                              rowsOfSubmappings
-                                = row.rowsOfSubmappings.map {
+                              rowsOfSubTables
+                                = row.rowsOfSubTables.map {
                                     case (m, rows)
                                       ⇒ m → updatedRows(m, rows)
                                   }
@@ -62,12 +73,12 @@ package object resultSet {
                         ( primaryKey → 
                           Row(
                               data
-                                = mapping.resultSetColumns
+                                = m.resultSetColumns
                                     .view
                                     .zipBy(value)
                                     .toMap,
-                              rowsOfSubmappings
-                                = mapping.subTableMappings
+                              rowsOfSubTables
+                                = m.subTableMappings
                                     .view
                                     .zipBy( updatedRows(_, Map()) )
                                     .toMap
@@ -83,7 +94,7 @@ package object resultSet {
 
               rs.beforeFirst()
               while ( rs.next() ) {
-                rows = updatedRows( mapping, rows )
+                rows = updatedRows( m, rows )
               }
               rs.close()
 
@@ -91,13 +102,51 @@ package object resultSet {
             }
 
 
-          def instances
-            ( rows : Map[PrimaryKey, Row] )
-            : Seq[_]
-            = ???
+          def value
+            ( m : Mapping,
+              row : Row )
+            : Any
+            = m match {
+                case m : mapping.Value
+                  ⇒ row.data(m.column)
+                case m : mapping.Option
+                  ⇒ Option( value( m.child.child, row ) )
+                case m : mapping.Tuple
+                  ⇒ m.reflection.instantiate(
+                      m.children
+                        .view
+                        .map( _.child )
+                        .map( value(_, row) )
+                    )
+                case m : mapping.Entity
+                  ⇒ m.reflection.instantiate(
+                      m.children
+                        .view
+                        .map( p ⇒ p.name → value(p.child, row) )
+                        .toMap
+                    )
+                case m : mapping.Seq
+                  ⇒ m.reflection.instantiate().asInstanceOf[Seq[_]] ++
+                    row.rowsOfSubTables(m).values
+                      .map( row ⇒ value( m.child.child, row) )
+                case m : mapping.Set
+                  ⇒ m.reflection.instantiate().asInstanceOf[Set[_]] ++
+                    row.rowsOfSubTables(m).values
+                      .map( row ⇒ value( m.child.child, row) )
+                case m : mapping.Map
+                  ⇒ m.reflection.instantiate().asInstanceOf[Map[_, _]] ++
+                    row.rowsOfSubTables(m).values
+                      .map( row ⇒ value( m.key.child, row ) → 
+                                  value( m.value.child, row ) )
+              }
 
+          def values
+            ( m : mapping.Table,
+              rows : Map[PrimaryKey, Row] )
+            = rows.values.map(value(m, _)).toList
+          
 
-          instances( fetchRowsAndClose() )
+          values( m, fetchRowsAndClose() )
 
         }
 

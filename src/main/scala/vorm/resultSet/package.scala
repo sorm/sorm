447423aa20ad924d4906a93804b3a51de4c94e77
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-20 14:20:29 +0400

    ideas

--- src/main/scala/vorm/resultSet/package.scala
+++ src/main/scala/vorm/resultSet/package.scala
@@ -12,32 +12,80 @@ package object resultSet {
   implicit class ResultSetExtensions
     ( rs : ResultSet )
     {
-      def results
-        [ T : TypeTag ]
+      def fetchInstances
+        [ T ]
         ( columnRoles : IndexedSeq[ColumnRole] )
         : Seq[T]
+        = fetchInstances( 
+              Set() + columnRoles.head.mapping.root,
+              columnRoles.view.zipWithIndex.toMap
+            )
+            .asInstanceOf[Seq[T]]
+
+      def fetchInstances
+        ( mappings : Set[Mapping],
+          indexesOfColumnRoles : Map[ColumnRole, Int] )
+        : Map[Mapping, Seq[_]]
         = {
-          def value
-            ( index : Int )
-            = columnRoles(index) match {
-                case ColumnRole.Value( mapping )
-                  ⇒ rs.value( index, mapping.column.jdbcType )
-                case ColumnRole.Hash( mapping )
-                  ⇒ rs.value( index, Column.Type.Integer.jdbcType )
+
+          case class Row
+            ( data : Map[ColumnRole, Any],
+              rowsOfSubmappings : RowGroupsOfMappings )
+
+          type RowGroup = Map[Seq[Any], Row]
+
+          type RowGroupsOfMappings = Map[Mapping, RowGroup]
+
+            
+          def fetchRows()
+            : RowGroupsOfMappings
+            = {
+
+              def value
+                ( role : ColumnRole )
+                // = rs.value( indexesOfColumnRoles(role), role.column.t.jdbcType )
+                = role match {
+                    case ColumnRole.Value( mapping )
+                      ⇒ rs.value( indexesOfColumnRoles(role), mapping.column.jdbcType )
+                    case ColumnRole.Hash( mapping )
+                      ⇒ rs.value( indexesOfColumnRoles(role), Column.Type.Integer.jdbcType )
+                  }
+
+              def updatedRows
+                ( mapping : Mapping,
+                  rows : RowGroupsOfMappings )
+                : RowGroupsOfMappings
+                = throw new NotImplementedError
+
+
+
+              var rows = RowGroupsOfMappings()
+
+              rs.beforeFirst()
+              while ( rs.next() ) {
+                for ( mapping ← mappings ) {
+                  rows = updatedRows( mapping, rows )
+                }
               }
+              rs.close()
 
-          val columnRolesWithIndexesStream
-            = columnRoles.toStream.zipWithIndex
+              rows
+            }
 
-          val rowData
-            : Stream[(Any, ColumnRole)]
-            = columnRolesWithIndexesStream
-                .map { case (role, i) ⇒ rs.getValue(i) → role }
 
-          
+          def instances
+            ( rows : RowGroupsOfMappings )
+            : Map[Mapping, Seq[_]]
+            = throw new NotImplementedError
+
+
+          instances( fetchRows() )
+
         }
 
     }
+
+
 }
 
 

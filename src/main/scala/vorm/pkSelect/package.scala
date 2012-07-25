0da7c7eba2502e8a644c5b88a19d7ea223669809
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-25 22:34:08 +0400

    pkselect on selectionNode

--- src/main/scala/vorm/pkSelect/package.scala
+++ src/main/scala/vorm/pkSelect/package.scala
@@ -6,6 +6,7 @@ import reflection._
 import structure._
 import query._
 import select._
+import selectionNode._
 
 package object pkSelect {
 
@@ -16,19 +17,34 @@ package object pkSelect {
         case Query.Where.Contains( m : mapping.Seq, v )
           ⇒ selectionNode( Query.Where.Includes(m, Seq(v)) )
         case Query.Where.Includes( m : mapping.Seq, v : Seq[_] )
+         // ⇒ SelectionNode.Select(
+         //       task
+         //         = SelectionNode.Task.PrimaryKey,
+         //       mapping
+         //         = m,
+         //       rows
+         //         = Some(0)
+         //     )
+         //     .foldFrom( v.view.map( Query.Where.Equals(m.child, _) ) ) {
+         //       (a, n)
+         //         ⇒ a.copy(
+         //               clause = a.clause.foldLeft(n)(SelectionNode.Or).some,
+         //               rows = a.rows.map(_ + 1)
+         //             )
+         //     }
           ⇒ SelectionNode.Select(
-                task 
-                  = SelectionNode.Task.PrimaryKey,
-                mapping 
-                  = m,
-                clause
-                  = v.view
-                      .map( Query.Where.Equals(m.child, _) )
-                      .map( selectionNode )
-                      .reduceOption( SelectionNode.Or ),
-                rows 
-                  = Some( v.size )
-              )
+                 task
+                   = SelectionNode.Task.PrimaryKey,
+                 mapping
+                   = m,
+                 clause
+                   = v.view
+                       .map( Query.Where.Equals(m.child, _) )
+                       .map( selectionNode )
+                       .reduceOption( SelectionNode.Or ),
+                 rows
+                   = Some( v.size )
+               )
         case Query.Where.Equals( m : mapping.Seq, v : Seq[_] )
           ⇒ SelectionNode.Select(
                 task 

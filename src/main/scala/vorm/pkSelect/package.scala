fc3b5597e3e2801baecd73371d55eb2dbcbbcdf9
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-25 07:10:23 +0400

    selectnode upd

--- src/main/scala/vorm/pkSelect/package.scala
+++ src/main/scala/vorm/pkSelect/package.scala
@@ -1,6 +1,7 @@
 package vorm
 
 import vorm._
+import extensions._
 import reflection._
 import structure._
 import query._
@@ -8,88 +9,58 @@ import select._
 
 package object pkSelect {
 
-  def pkSelect
-    ( q : Query )
-    : Select
-    = {
-      val m = q.mapping
+  def selectionNode
+    ( n : Query.Where )
+    : SelectionNode
+    = n match {
+        case Query.Where.Contains( m : mapping.Seq, v )
+          ⇒ selectionNode( Query.Where.Includes(m, Seq(v)) )
+        case Query.Where.Includes( m : mapping.Seq, v : Seq[_] )
+          ⇒ SelectionNode.Select(
+                task 
+                  = SelectionNode.Task.PrimaryKey,
+                mapping 
+                  = m,
+                clause
+                  = v.view
+                      .map( Query.Where.Equals(m.child, _) )
+                      .map( selectionNode )
+                      .reduceOption( SelectionNode.Or ),
+                rows 
+                  = Some( v.size )
+              )
+        case Query.Where.Equals( m : mapping.Seq, v : Seq[_] )
+          ⇒ SelectionNode.Select(
+                task 
+                  = SelectionNode.Task.PrimaryKey,
+                mapping 
+                  = m,
+                clause
+                  = v.view
+                      .map( Query.Where.Equals(m.child, _) )
+                      .map( selectionNode )
+                      .reduceOption( SelectionNode.Or )
+                      .foldRight( 
+                          selectionNode( Query.Where.HasSize( m, v.size ) )
+                        )( SelectionNode.And )
+                      .some,
+                rows 
+                  = Some( v.size )
+              )
+        case Query.Where.HasSize( m : mapping.Table, v : Int )
+          ⇒ SelectionNode.Select(
+                task 
+                  = SelectionNode.Task.PrimaryKey,
+                mapping 
+                  = m,
+                rows 
+                  = Some( v )
+              )
+        case Query.Where.Equals( m : mapping.Value, v )
+          ⇒ SelectionNode.Equals( m, v )
 
-      def select
-        ( n : Query.Where )
-        : Select
-        = {
-          val alias
-            : mapping.Table ⇒ String
-            = new collection.mutable.HashMap[mapping.Table, String]() {
-                override def default
-                  ( m : mapping.Table )
-                  = {
-                    val n = "t" + size
-                    update(m, n)
-                    n
-                  }
-              }
+      }
 
-          def basis
-            = Select(
-                  // groupBy = GroupBy(n.mapping.root.primaryKey)
-                )
-          n match {
-            case Query.Where.And( left, right )
-              ⇒ intersect( select(left), select(right) )
-            case Query.Where.Or( left, right )
-              ⇒ union( select(left), select(right) )
-            case Query.Where.Includes( m : mapping.Seq, v : Seq[_] )
-              ⇒ val s
-                  = select(
-                        v.view
-                          .map(Query.Where.Equals(m.item, _))
-                          .reduce(Query.Where.Or)
-                      )
-                s.copy(
-                    having 
-                      = s.having ++:
-                        Condition( 
-                            Left(
-                                Count(
-                                    m.primaryKeyColumns.view
-                                      .map( _.name )
-                                      .map( Column( _, Some( alias(m) ) ) ),
-                                    true
-                                  )
-                              ),
-                            ConditionRelation.Equal,
-                            Right(v.size)
-                          ) ::
-                        Nil reduceOption Clause.And
-                          // .foldTo(s.having)(Clause.And)
-                  )
-
-            // case Query.WhereNode.Equals( m : mapping.Value, v )
-            //   ⇒ sql.WhereNode.Equals(
-            //       alias(m.parentTableMapping) + "." + m.column.name,
-            //       (v, m.column.t.jdbcType)
-            //     )
-            // case Query.WhereNode.Equals( m : mapping.Tuple, v : Product ) 
-            //   ⇒ m.children
-            //       .view
-            //       .map( m ⇒ m.child -> m.index )
-            //       .map{ 
-            //         case (m, i) 
-            //           ⇒ Query.WhereNode.Equals( m, v.productElement(i) ) 
-            //       }
-            //       .map( where )
-            //       .reduce( sql.WhereNode.And )
-            // case Query.WhereNode.Equals( m : mapping.Option, v : Option[_] )
-            //   ⇒ v match {
-            //       case Some(v) 
-            //         ⇒ where( Query.WhereNode.Equals(m.child.child, v) )
-            //       case None
-            //         ⇒ where( Query.WhereNode.Equals(m.child.child, null) )
-            //     }
-          }
-        }
-    }
 
 
 }

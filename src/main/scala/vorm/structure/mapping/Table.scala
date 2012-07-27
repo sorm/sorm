fde64391dc44ef5ef9356d62521b1a52b2f07d11
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-27 18:27:17 +0400

    mapping select

--- src/main/scala/vorm/structure/mapping/Table.scala
+++ src/main/scala/vorm/structure/mapping/Table.scala
@@ -3,6 +3,8 @@ package vorm.structure.mapping
 import vorm._
 import structure._
 import reflection._
+import extensions._
+import vorm.{sql => Sql}
 
 trait Table
   extends Mapping
@@ -34,4 +36,79 @@ trait Table
     lazy val ownerTableMapping
       : Table
       = ???
+
+
+
+    lazy val ownerTable
+      : Table
+      = ???
+
+    lazy val deepSubtableMappings
+      : scala.Seq[Table]
+      = subTableMappings.flatMap( _.deepSubtableMappings )
+
+    lazy val skeletonSelectTableAliases
+      : collection.immutable.Map[Mapping, String]
+      = deepSubtableMappings.view
+          .+:(this)
+          .zipWithIndex
+          .map{ case (m, i) ⇒ m → "t" + i }
+          .toMap
+
+    lazy val skeletonSelect
+      : Sql.Select
+      = {
+        Sql.Select(
+            what 
+              = Vector(),
+            from 
+              = Sql.From(
+                    Sql.Table( tableName ),
+                    Some( skeletonSelectTableAliases(this) )
+                  )
+          )
+          .foldFrom( deepSubtableMappings ) { (s, m) ⇒ 
+            s.copy(
+                join
+                  = Sql.Join(
+                        what  
+                          = Sql.Table( m.tableName ),
+                        as    
+                          = Some( skeletonSelectTableAliases(m) ),
+                        on    
+                          = m.ownerTableColumnMappings
+                              .map { case (l, r) ⇒ 
+                                Sql.Column(l, Some(skeletonSelectTableAliases(m))) → 
+                                Sql.Column(r, Some(skeletonSelectTableAliases(m.ownerTable)))
+                              }
+                      ) +:
+                    s.join
+              )
+          }
+      }
+
+    lazy val resultSetSelect
+      : ( Sql.Select, scala.Seq[(ddl.Column, Table)] )
+      = skeletonSelect
+          .foldFrom( this +: deepSubtableMappings ) { 
+            ( s, m ) 
+            ⇒ s.copy(
+                  what 
+                    = m.resultSetColumns.view
+                        .map( _.name )
+                        .map( sql.Column(_, Some(skeletonSelectTableAliases(m))) ) ++:
+                      s.what
+                )
+          } → 
+        resultSetColumns.map( _ → this )
+
+    lazy val primaryKeySelect
+      : Sql.Select
+      = skeletonSelect.copy(
+            what
+              = primaryKeyColumns.view
+                  .map( _.name )
+                  .map( Sql.Column(_, Some(skeletonTablesAliases(this))) )
+                  .toList
+          )
   }

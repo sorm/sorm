bcbb427a386e4a4b1b993110263b0b123c76ab35
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-27 22:22:38 +0400

    upd

--- src/main/scala/vorm/selectAbstraction/MappingSelect.scala
+++ src/main/scala/vorm/selectAbstraction/MappingSelect.scala
@@ -5,6 +5,7 @@ import extensions._
 import structure._
 import query._
 import mapping.{Table => TableMapping, Seq => SeqMapping}
+import vorm.{sql => Sql}
 
 /**
  *  Usage:
@@ -19,12 +20,64 @@ import mapping.{Table => TableMapping, Seq => SeqMapping}
  */
 case class MappingSelect
   ( mapping : TableMapping,
-    subSelects : Map[MappingSelect, String] )
+    // mappingsInSkeleton : Set[TableMapping],
+    skeletonAliases : Map[TableMapping, String],
+
+    sql : Sql.Select )
+    // skeletonJoins : Map[TableMapping, Sql.Join] )
   {
+
+    private def withSkeletonTo
+      ( m : TableMapping )
+      : MappingSelect
+      = ???
+
+    /**
+     * This select with new select joined, but not binded  
+     */
+    private def withSelect
+      ( s : MappingSelect )
+      : ( MappingSelect, String )
+      = copy(
+            sql
+              = sql.copy(
+                    join
+                      = sql.join :+
+                        Sql.Join( s.sql, Some("t" + sql.join.length),
+                              kind = Sql.JoinKind.Inner )
+                  )
+          ) → 
+        "t" + sql.join.length
+
 
     def andWhere
       ( w : Query.Where )
-      = this intersection MappingSelect(w)
+      // = this intersection MappingSelect(w)
+      = MappingSelect(w) match {
+          case Some(s : MappingSelect) ⇒ 
+            withSkeletonTo(s.mapping).withSelect(s) match { 
+                case (s1, a) ⇒ 
+                  s1.copy(
+                      sql
+                        = s1.sql.copy(
+                              where
+                                = ( s1.sql.where ++
+                                    s.mapping.primaryKeyColumns.view
+                                      .map(_.name)
+                                      .map{ n ⇒ 
+                                        Sql.Clause.Equals(
+                                            Sql.Column(n, Some(a)),
+                                            Sql.Column(n, Some(skeletonAliases(s.mapping)))
+                                          )
+                                      }
+                                      .reduceOption( Sql.Clause.And )
+                                    )
+                                    .reduceOption( Sql.Clause.And )
+                            )
+                    )
+              }
+        }
+
 
     def orWhere
       ( w : Query.Where )
@@ -53,17 +106,18 @@ object MappingSelect {
 
   def apply
     ( w : Query.Where )
-    = apply( Clause(w) )
-
-  def apply
-    ( c : Clause.Filter )
-    = c match {
-        case Clause.Select ⇒ 
-          MappingSelect(
-              subSelects
-                = ???
-            )
+    = w match {
+        case Query.Where.Equals( m : mapping.Value, v ) ⇒ 
+          ???
+        case Query.Where.Equals( m : SeqMapping, v : Seq[_] ) ⇒ 
+          ???
+          // ⇒ m.select.primaryKey
+          //     .foldFrom(v) { (s, v) ⇒ 
+          //       s.orWhere(
+          //           Query.Where.Equals(m.child.child, v)
+          //         )
+          //     }
       }
-  
+
 }
 

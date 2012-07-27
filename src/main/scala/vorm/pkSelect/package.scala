fce4992b9748ead66b8091b92dd3ab7d841e5e9d
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-27 19:28:06 +0400

    Clause factored out

--- src/main/scala/vorm/pkSelect/package.scala
+++ src/main/scala/vorm/pkSelect/package.scala
@@ -9,49 +9,6 @@ import vorm.{sql => Sql}
 
 package object pkSelect {
 
-  def clause
-    ( n : Query.Where )
-    : Clause
-    = n match {
-        case Query.Where.Contains( m : mapping.Seq, v )
-          ⇒ clause( Query.Where.Includes(m, Seq(v)) )
-        case Query.Where.Includes( m : mapping.Seq, v : Seq[_] )
-          ⇒ Clause.Select(
-                 mapping
-                   = m,
-                 clause
-                   = v.view
-                       .map( Query.Where.Equals(m.child, _) )
-                       .map( clause )
-                       .reduceOption( Clause.Or ),
-                 rows
-                   = Some( v.length )
-               )
-        case Query.Where.Equals( m : mapping.Seq, v : Seq[_] )
-          ⇒ Clause.Select(
-                mapping 
-                  = m,
-                clause
-                  = v.view
-                      .map( Query.Where.Equals(m.child, _) )
-                      .map( clause )
-                      .reduceOption( Clause.Or )
-                      .foldRight( 
-                          clause( Query.Where.HasSize( m, v.length ) )
-                        )( Clause.And )
-                      .some,
-                rows 
-                  = Some( v.length )
-              )
-        case Query.Where.HasSize( m : mapping.Table, v : Int )
-          ⇒ Clause.Select( m, None, Some(v) )
-        case Query.Where.Equals( m : mapping.Value, v )
-          ⇒ Clause.Equals( m, v )
-
-      }
-
-
-
 
   def select
     ( c : Clause.Select )
@@ -64,13 +21,7 @@ package object pkSelect {
         = ???
 
 
-      c.mapping.selectSkeleton.copy(
-          what 
-            = c.mapping.primaryKeyColumns.view
-                .map( _.name )
-                .map( Sql.Column(_, Some(c.mapping.skeletonTablesAliases(c.mapping))) )
-                .toList
-        )
+      c.mapping.primaryKeySelect
         .foldFrom(c.rows) {
           (s, r)
             ⇒ s.copy(

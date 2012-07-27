1f0351ac99481444ba78eb9c87fc9e7100c7bc7e
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-07-28 01:03:23 +0400

    upd

--- src/main/scala/vorm/selectAbstraction/MappingSelect.scala
+++ src/main/scala/vorm/selectAbstraction/MappingSelect.scala
@@ -4,7 +4,7 @@ import vorm._
 import extensions._
 import structure._
 import query._
-import mapping.{Table => TableMapping, Seq => SeqMapping}
+import mapping.{Table => TableMapping, Seq => SeqMapping, Value => ValueMapping}
 import vorm.{sql => Sql}
 
 /**
@@ -20,95 +20,125 @@ import vorm.{sql => Sql}
  */
 case class MappingSelect
   ( mapping : TableMapping,
-    // mappingsInSkeleton : Set[TableMapping],
-    skeletonAliases : Map[TableMapping, String],
-
-    sql : Sql.Select )
-    // skeletonJoins : Map[TableMapping, Sql.Join] )
+    skeletonAliases : Map[TableMapping, String] = Map(),
+    joins : Vector[Sql.Join] = Vector(),
+    where : Option[Sql.Clause] = None,
+    groupBy : Vector[Sql.Column] = Vector(),
+    having : Option[Sql.Clause] = None )
   {
+    lazy val resultSetSql
+      : Sql.Select
+      = ???
+
+    lazy val primaryKeySql
+      : Sql.Select
+      = ???
+
+    private lazy val newAlias
+      = "t" + joins.length
 
     private def withSkeletonTo
-      ( m : TableMapping )
+      ( m : Mapping )
       : MappingSelect
-      = ???
-
-    /**
-     * This select with new select joined, but not binded  
-     */
-    private def withSelect
-      ( s : MappingSelect )
-      : ( MappingSelect, String )
-      = copy(
-            sql
-              = sql.copy(
-                    join
-                      = sql.join :+
-                        Sql.Join( s.sql, Some("t" + sql.join.length),
-                              kind = Sql.JoinKind.Inner )
-                  )
-          ) → 
-        "t" + sql.join.length
-
-
-    def andWhere
-      ( w : Query.Where )
-      // = this intersection MappingSelect(w)
-      = MappingSelect(w) match {
-          case Some(s : MappingSelect) ⇒ 
-            withSkeletonTo(s.mapping).withSelect(s) match { 
-                case (s1, a) ⇒ 
-                  s1.copy(
-                      sql
-                        = s1.sql.copy(
-                              where
-                                = ( s1.sql.where ++
-                                    s.mapping.primaryKeyColumns.view
-                                      .map(_.name)
-                                      .map{ n ⇒ 
-                                        Sql.Clause.Equals(
-                                            Sql.Column(n, Some(a)),
-                                            Sql.Column(n, Some(skeletonAliases(s.mapping)))
-                                          )
-                                      }
-                                      .reduceOption( Sql.Clause.And )
-                                    )
-                                    .reduceOption( Sql.Clause.And )
-                            )
-                    )
-              }
+      = m match {
+          case m : TableMapping ⇒ 
+            if( m == mapping )
+              this
+            else if( skeletonAliases contains m )
+              this
+            else { 
+              val s = withSkeletonTo( m.parentTableMapping.get )
+              s.copy(
+                  skeletonAliases 
+                    = s.skeletonAliases + (m → s.newAlias),
+                  joins 
+                    = s.joins :+ 
+                      Sql.Join( 
+                          Sql.Table(m.tableName),
+                          Some(s.newAlias),
+                          m.primaryKeyColumns.view
+                            .map(_.name)
+                            .map{ n ⇒ 
+                              Sql.Column(n, Some(s.newAlias)) → 
+                              Sql.Column(n, Some(s.skeletonAliases(m.parentTableMapping.get)))
+                            }
+                            .toList
+                        )
+                )
+            }
+          case _ ⇒
+            withSkeletonTo( m.parentTableMapping.get )
         }
 
+    private def withSelect
+      ( s : MappingSelect, 
+        o : (Sql.Clause, Sql.Clause) => Sql.Clause )
+      : MappingSelect
+      = copy(
+            joins
+              = joins :+
+                Sql.Join( s.primaryKeySql, Some(newAlias),
+                          kind = Sql.JoinKind.Inner ),
+            where
+              = ( where ++
+                  s.mapping.primaryKeyColumns.view
+                    .map{ _.name }
+                    .map{ n ⇒ 
+                        Sql.Clause.Equals(
+                            Sql.Column(n, Some(newAlias)),
+                            Sql.Column(n, Some(skeletonAliases(s.mapping)))
+                          )
+                      } 
+                    .reduceOption{ Sql.Clause.And } )
+                  .reduceOption{ o }
+          )
+
+
+    // def withWhere
+    //   ( c : Option[Sql.Clause],
+    //     o : (Sql.Clause, Sql.Clause) => Sql.Clause )
+    //   = copy( where = ( where ++ c ) reduceOption o )
+
+
+    def withWhere
+      ( w : Query.Where.Filter, 
+        o : (Sql.Clause, Sql.Clause) => Sql.Clause )
+      = MappingSelect(w)
+          .map{ withSelect(_, o) }
+          .getOrElse{ 
+            copy( 
+                where
+                  = ( where ++ condition(w) ) 
+                      .reduceOption(o)
+              )
+          }
+
+    private def condition
+      ( w : Query.Where.Filter )
+      : Option[Sql.Clause]
+      = w match {
+          case Query.Where.Equals( m : ValueMapping, v ) ⇒ 
+            Sql.Clause.Equals( 
+                Sql.Column( m.columnName, 
+                            Some( skeletonAliases(m.parentTableMapping.get) ) ),
+                Sql.Value( v )
+              )
+              .some
+        }
 
     def orWhere
-      ( w : Query.Where )
-      = this union MappingSelect(w)
+      ( w : Query.Where.Filter )
+      = withSkeletonTo(w.mapping)
+          .withWhere(w, Sql.Clause.Or)
 
-    /**
-     * Mixes two selects with optimizations
-     */
-    private def intersection
-      ( s : MappingSelect )
-      : MappingSelect
-      = ???
-
-    private def union
-      ( s : MappingSelect )
-      : MappingSelect
-      = ???
-
-    private lazy val referredSubTables
-      : Seq[TableMapping]
-      = ???
 
   }
 
 object MappingSelect {
-
   def apply
     ( w : Query.Where )
+    : Option[MappingSelect]
     = w match {
-        case Query.Where.Equals( m : mapping.Value, v ) ⇒ 
-          ???
         case Query.Where.Equals( m : SeqMapping, v : Seq[_] ) ⇒ 
           ???
           // ⇒ m.select.primaryKey

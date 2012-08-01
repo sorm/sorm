64131695db179d6eb0381d5c11ad2572884c3c76
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-01 13:34:11 +0400

    init upd

--- src/test/scala/vorm/sql/Sandbox.scala
+++ src/test/scala/vorm/sql/Sandbox.scala
@@ -1,5 +1,8 @@
 package vorm.sql
 
+import vorm._
+import extensions._
+
 object Sandbox extends App {
   
 
--- src/test/scala/vorm/sql/Test.scala
+++ src/test/scala/vorm/sql/Test.scala
@@ -5,6 +5,9 @@ import org.scalatest.matchers.ShouldMatchers
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
+import vorm._
+import extensions._
+
 @RunWith(classOf[JUnitRunner])
 class Test extends FunSuite with ShouldMatchers {
   test("sql") {

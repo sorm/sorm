cd259857db9e8a013ef24ee56f88ebc0eea3f2e7
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-02 14:45:36 +0400

    structure: upd

--- src/main/scala/vorm/structure/Membership.scala
+++ src/main/scala/vorm/structure/Membership.scala
@@ -4,34 +4,33 @@ sealed trait Membership[ParentT] {
   def parent: ParentT
 }
 
-object Membership
-  {
-    sealed case class EntityProperty
-      [ ParentT ]
-      ( name : String, parent : ParentT )
-      extends Membership[ParentT]
-    sealed case class TupleItem
-      [ ParentT ]
-      ( index : Int, parent : ParentT )
-      extends Membership[ParentT]
-    sealed case class OptionItem
-      [ ParentT ]
-      ( parent : ParentT )
-      extends Membership[ParentT]
-    sealed case class SeqItem
-      [ ParentT ]
-      ( parent : ParentT )
-      extends Membership[ParentT]
-    sealed case class SetItem
-      [ ParentT ]
-      ( parent : ParentT )
-      extends Membership[ParentT]
-    sealed case class MapKey
-      [ ParentT ]
-      ( parent : ParentT )
-      extends Membership[ParentT]
-    sealed case class MapValue
-      [ ParentT ]
-      ( parent : ParentT )
-      extends Membership[ParentT]
-  }
+object Membership {
+  sealed case class EntityProperty
+    [ ParentT ]
+    ( name : String, parent : ParentT )
+    extends Membership[ParentT]
+  sealed case class TupleItem
+    [ ParentT ]
+    ( index : Int, parent : ParentT )
+    extends Membership[ParentT]
+  sealed case class OptionItem
+    [ ParentT ]
+    ( parent : ParentT )
+    extends Membership[ParentT]
+  sealed case class SeqItem
+    [ ParentT ]
+    ( parent : ParentT )
+    extends Membership[ParentT]
+  sealed case class SetItem
+    [ ParentT ]
+    ( parent : ParentT )
+    extends Membership[ParentT]
+  sealed case class MapKey
+    [ ParentT ]
+    ( parent : ParentT )
+    extends Membership[ParentT]
+  sealed case class MapValue
+    [ ParentT ]
+    ( parent : ParentT )
+    extends Membership[ParentT]
+}

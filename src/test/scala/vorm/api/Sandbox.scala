2b2cdee76acf1b32dc0a83b225470e58a5183107
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-03 20:25:58 +0400

    ideas

--- src/test/scala/vorm/api/Sandbox.scala
+++ src/test/scala/vorm/api/Sandbox.scala
@@ -15,4 +15,58 @@ object Sandbox extends App {
 
   val artist = api.all[Artist].filterEquals("name", "Metallica").offset(0).limit(1).head
 
+
+  //  ideas:::
+  // object DB extends Instance(
+  //   Settings("jdbc://")
+  //     .withEntity[Artist](primaryKey = Seq("id"), autoIncrement = Set("id"))
+  //     .withEntity[Style]
+  //     .withEntity[Name]
+  //     .withEntity[Locale]
+  // )
+
+  object DB 
+    extends Instance(
+      url 
+        = "jdbc://",
+      user 
+        = "",
+      password 
+        = "",
+      entities 
+        = Entity[Artist](primaryKey = Seq("id"), autoIncrement = Set("id")) ::
+          Entity[Style]() ::
+          Entity[Name]() ::
+          Entity[Locale]() ::
+          Nil
+    )
+
+  DB.save(
+    DB.one[Locale].filter("code" equal "ru").get.copy(code = "ru")
+  )
+
+  sealed case class Entity
+    [ T : TypeTag ]
+    ( primaryKey    : Seq[String],
+      uniqueKeys    : Set[Seq[String]]  = Set.empty,
+      indexes       : Set[Seq[String]]  = Set.empty,
+      autoIncrement : Set[String]       = Set.empty )
+    {
+      // here should be tests on validity of provided data
+    }
+  object Entity {
+    def apply
+      [ T : TypeTag ]
+      ( uniqueKeys    : Set[Seq[String]]  = Set.empty,
+        indexes       : Set[Seq[String]]  = Set.empty,
+        autoIncrement : Set[String]       = Set.empty )
+      : Entity[T]
+      = apply[T](
+          primaryKey = Seq("id"),
+          autoIncrement = autoIncrement + "id",
+          uniqueKeys = uniqueKeys,
+          indexes = indexes
+        )
+  }
+
 }

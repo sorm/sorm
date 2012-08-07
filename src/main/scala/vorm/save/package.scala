009465bdb0afcae179f11b5753ba02c872c717a3
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-07 13:08:03 +0400

    upd

--- src/main/scala/vorm/save/package.scala
+++ src/main/scala/vorm/save/package.scala
@@ -9,33 +9,6 @@ import extensions._
 
 package object save {
 
-  // class Mapped
-  //   ( val value : Any,
-  //     val mapping : Mapping )
-  //   {
-  //     def columnValues = ???
-
-  //     def children
-  //       = mapping.children
-
-  //   }
-
-  // case class SaveQuery
-  //   ( value : Any,
-  //     mapping : Mapping )
-  //   {
-  //   }
-
-  // class MappedEntity
-  //   ( val value : AnyRef,
-  //     val mapping : EntityMapping )
-  //   {
-  //     lazy val properties
-  //       = mapping.properties
-  //           .map{ case (n, p) ⇒ Mapped( mapping.reflection.propertyValue(n), p ) }
-  //   }
-
-
   /**
    * This implementation is based on strict rules about how statements are resolved to whether be Update or Insert. These are:
    * 1. All entities must have an autoincremented id property
@@ -44,41 +17,69 @@ package object save {
    * 4. All rows of collection tables are deleted before update and thus should result in no such conflicts
    */
 
+  // def statements
+  //   ( v : Any,
+  //     m : EntityMapping )
+  //   = {
+
+  //     val values
+  //       : Map[TableMapping, AnyRef]
+  //       = {
+  //         def childrenValues 
+  //           ( v : Any,
+  //             m : TableMapping )
+  //           : Map[TableMapping, AnyRef]
+  //           = m match {
+  //               case m : EntityMapping =>
+  //                 m.properties.map{ case (n, p) ⇒ 
+  //                   childrenValues( m.reflection.propertyValue(v, n), p ) +
+  //                   ( p)
+  //                 }
+  //             }
+
+  //         childrenValues(v, m)
+  //       }
+
+  //     def rows
+  //       ( v : Any,
+  //         m : TableMapping )
+  //       : Seq[Map[String, JdbcValue]]
+  //       = 
+
+
+  //     def allMappings : Seq[TableMapping]
+  //     def value ( m : TableMapping ) : Any
+
+
+  //     allMappings
+  //       .orderedByDominance
+  //       .
+
+  //   }
+
   def statements
     ( v : Any,
-      m : EntityMapping )
-    = {
-      // def columnValues
-      //   ( m : Mapping )
+      m : TableMapping )
+    : Vector[Statement]
+    = (v, m) match {
+        case (v : Set[_], m : SetMapping) ⇒ 
+          m.item match {
+            case i : CollectionMapping ⇒ 
+              v.flatMap{ v ⇒ rowStatement(v, m) +: statements(v, i) } 
+            case _ ⇒ 
+              v.flatMap{ v ⇒ statements(v, i) :+ rowStatement(v, i) }
+          }
+        case (v : Product, m : TupleMapping) ⇒ 
+          m.items
+            .view
+            .zipWithIndex
+            .flatMap{ case (m, i) ⇒ 
+              statements( v.productElement(i), m )
+            }
+        case (_, _ : ValueMapping) ⇒ 
+          Vector.empty
+      }
 
-      // val rows 
-      //   : Map[TableMapping, Seq[Map[String, JdbcValue]]]
-      //   = 
-      lazy val isPersisted : Boolean
-        = ???
-
-      val values
-        : Map[TableMapping, AnyRef]
-        = ???
-
-      def rows
-        ( v : Any,
-          m : TableMapping )
-        : Seq[Map[String, JdbcValue]]
-        = 
-
-
-      def allMappings : Seq[TableMapping]
-      def value ( m : TableMapping ) : Any
-
-
-      allMappings
-        .orderedByDominance
-        .
-
-    }
-
-
-
+////  shit, we need the generated columns
 
 }

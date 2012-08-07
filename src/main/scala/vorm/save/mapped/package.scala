1359ff183808a631d3e6d7fae16f2011b58cb1e1
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-07 15:22:00 +0400

    init

---
+++ src/main/scala/vorm/save/mapped/package.scala
@@ -0,0 +1,59 @@
+package vorm.save
+
+import vorm._
+import structure._
+import mapping._
+import jdbc._
+import sql._
+import extensions._
+
+package object mapped {
+
+  trait Mapped {
+    def value : Any
+    def mapping : Mapping
+  }
+  object Mapped {
+    def apply
+      ( value : Any,
+        mapping : Mapping )
+      : Mapped
+      = ???
+  }
+
+  class MappedEntity
+    ( val value : AnyRef,
+      val mapping : EntityMapping )
+    {
+      lazy val properties
+        : Map[String, Mapped]
+        = mapping.properties
+            .map{ case (n, m) ⇒ 
+              n → Mapped(mapping.reflection.propertyValue(n, value), m) 
+            }
+    }
+  
+  class MappedTuple
+    ( val value : Product,
+      val mapping : TupleMapping )
+    {
+      def items
+        = mapping.items
+            .view
+            .zipWithIndex
+            .map{ case (m, i) ⇒ Mapped(value.productElement(i), m) }
+    }
+  
+  class MappedSeq
+    ( val value : Seq[_],
+      val mapping : SeqMapping )
+    {
+      def items
+        : Seq[Mapped]
+        = value.map{ Mapped(_, mapping.item) }
+    }
+
+
+
+
+}

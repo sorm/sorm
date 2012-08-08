5a589175dc3da687a83a2d294062fb18d960412f
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-09 00:40:12 +0400

    make it compile

--- src/main/scala/vorm/api/Entity.scala
+++ src/main/scala/vorm/api/Entity.scala
@@ -3,9 +3,9 @@ package vorm.api
 sealed case class Entity
   [ T : TypeTag ]
   ( primaryKey    : Seq[String],
-    uniqueKeys    : Set[Seq[String]]  = Set.empty,
-    indexes       : Set[Seq[String]]  = Set.empty,
-    autoIncrement : Set[String]       = Set.empty )
+    uniqueKeys    : Set[Seq[String]],
+    indexes       : Set[Seq[String]],
+    autoIncrement : Set[String] )
   {
     // here should be tests on validity of provided data
   }
@@ -16,7 +16,7 @@ object Entity {
       indexes       : Set[Seq[String]]  = Set.empty,
       autoIncrement : Set[String]       = Set.empty )
     : Entity[T]
-    = apply[T](
+    = new Entity[T](
         primaryKey = Seq("id"),
         autoIncrement = autoIncrement + "id",
         uniqueKeys = uniqueKeys,

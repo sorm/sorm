b18d3f6f040b6ba9e318402143ccc42f152c5b47
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-09 00:35:01 +0400

    upd

--- src/main/scala/vorm/jdbc/package.scala
+++ src/main/scala/vorm/jdbc/package.scala
@@ -2,6 +2,7 @@ package vorm
 
 import com.weiglewilczek.slf4s.Logger
 import java.sql.{ResultSet, PreparedStatement, Connection, Statement => JStatement}
+import org.joda.time.DateTime
 
 import vorm._
 import extensions._
@@ -15,10 +16,33 @@ package object jdbc {
 
 
   type JdbcType = Int
+  object JdbcType {
+    import java.sql.Types._
+    /**
+     * Shouldn't really be used
+     */
+    def apply ( v : Any )
+      = v match {
+          case _ : String     => VARCHAR
+          case _ : BigDecimal => DECIMAL
+          case _ : Boolean    => BIT
+          case _ : Byte       => TINYINT
+          case _ : Short      => SMALLINT
+          case _ : Int        => INTEGER
+          case _ : Long       => BIGINT
+          case _ : Float      => REAL
+          case _ : Double     => DOUBLE
+          case _ : DateTime   => ???
+          case _              => ???
+        }
+  }
 
   case class JdbcValue
     ( value : Any, 
       t : JdbcType )
+  object JdbcValue {
+    def apply ( v : Any ) : JdbcValue = apply(v, JdbcType(v))
+  }
   
   case class Statement
     ( sql: String,

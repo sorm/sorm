8a7931f21534ae40b96e6786c863cc66a86f61d2
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-08 18:03:08 +0400

    jdbc: ConnectionAdapterSimulator correction

--- src/test/scala/vorm/jdbc/ConnectionAdapterSimulator.scala
+++ src/test/scala/vorm/jdbc/ConnectionAdapterSimulator.scala
@@ -3,8 +3,8 @@ package vorm.jdbc
 import com.weiglewilczek.slf4s.Logging
 import java.sql.{Connection, Statement => JStatement}
 
-trait ConnectionAdapterSimulator
-  extends ConnectionAdapter
+class ConnectionAdapterSimulator
+  extends ConnectionAdapter(null)
   with Logging
   {
 
@@ -12,14 +12,14 @@ trait ConnectionAdapterSimulator
       ( stmt : Statement )
       : List[IndexedSeq[Any]] 
       = {
-        logger.trace(stmt.toString)
-        List(Vector(777))
+        logger.info(stmt.toString)
+        List(Vector(777l))
       }
 
     override def executeUpdate
       ( stmt : Statement )
       : Int = {
-        logger.trace(stmt.toString)
+        logger.info(stmt.toString)
         1
       }
 

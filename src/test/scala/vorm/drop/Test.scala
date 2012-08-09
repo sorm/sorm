4cba3baae6d44bece8a1be807390e065e4254ea3
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-09 06:21:50 +0400

    drop

---
+++ src/test/scala/vorm/drop/Test.scala
@@ -0,0 +1,13 @@
+package vorm.drop
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Test extends FunSuite with ShouldMatchers {
+  test("simulation") {
+
+  }
+}

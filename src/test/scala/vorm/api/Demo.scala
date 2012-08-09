5507cd923f3bc16bd2d58d544985934f909f63db
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-10 01:25:34 +0400

    a common sample db

--- src/test/scala/vorm/api/Demo.scala
+++ src/test/scala/vorm/api/Demo.scala
@@ -1,64 +1,24 @@
 package vorm.api
 
-import org.joda.time._
-
 import vorm._
+import samples._
 import extensions._
+
 import com.codahale.logula.Logging
 import org.apache.log4j.Level
 
 object Demo extends App {
-
-  import samples.ArtistModel._
 
   Logging.configure { log =>
     log.level = Level.TRACE
     log.loggers("vorm.jdbc.ConnectionAdapter") = Level.WARN
   }
 
-  object Db
-    extends Instance(
-      url
-        = "jdbc:h2:mem:test",
-//        = "jdbc:mysql://localhost/test",
-      user
-        = "",
-      password
-        = "",
-      entities
-        = Seq( Entity[Artist](), Entity[Style](), Entity[Name](), Entity[Locale]() ),
-      mode
-        = Mode.DropAllCreate
-    )
+  import SampleDb._
+  populateDb()
 
-  val ru
-    = Db.save( Locale("ru") )
-  val en
-    = Db.save( Locale("en") )
-  val rock
-    = Db.save( Style(Name(en, "Rock") :: Nil) )
-  val hardRock
-    = Db.save( Style(Name(en, "Hard Rock") :: Name(ru, "Тяжёлый рок") :: Nil) )
-  val metallica
-    = Db.save( Artist( Seq(Name(en, "Metallica")), Set(rock, hardRock) ) )
-  val nirvana
-    = Db.save( Artist( Seq(Name(en, "Nirvana")), Set(rock, hardRock) ) )
-  val kino
-    = Db.save( Artist( Seq(Name(ru, "Кино"), Name(en, "Kino")), Set(rock) ) )
-
-//  kino.id.println()
-//  kino.println()
-//  Db.query[Name].filterEquals("value", "Rock").all.println()
-  Db.query[Artist].all.map{_.names.head.value}.println()
+  Db.query[Artist].all.map{_.names.values.head.head}.println()
   Db.query[Artist].all.map{_.id}.println()
-//  val artist = api.all[Artist].filterEquals("name", "Metallica").offset(0).limit(1).head
-//
-//  val style
-//    = db.one[Style].where(
-//        ("names.value" === "pop") and
-//        ("names.locale.code" === "en")
-//      )
-//
 
 
 }
---
+++ src/test/scala/vorm/samples/SampleDb.scala
@@ -0,0 +1,111 @@
+package vorm.samples
+
+import vorm._
+import api._
+import extensions._
+
+object SampleDb {
+  
+  // A model:
+
+  case class Artist
+    ( names : Map[Locale, Seq[String]],
+      styles : Set[Style] )
+
+  case class Style
+    ( names : Map[Locale, Seq[String]] )
+
+  case class Locale
+    ( code : String )
+
+
+  //  A db instance:
+
+  object Db
+    extends Instance(
+      entities
+        = Seq( Entity[Artist](), 
+               Entity[Style](), 
+               Entity[Locale]() ),
+      url
+        = "jdbc:h2:mem:test",
+      mode
+        = Mode.DropAllCreate
+    )
+
+
+  def populateDb() {
+
+    //  Fill up the db with sample data:
+
+    val ru
+      = Db.save( Locale("ru") )
+    val en
+      = Db.save( Locale("en") )
+
+    val rock
+      = Db.save( Style( Map(en -> Seq("Rock"),
+                            ru -> Seq("Рок")) ) )
+    val hardRock
+      = Db.save( Style( Map(en -> Seq("Hard Rock"),
+                            ru -> Seq("Тяжёлый рок", "Тяжелый рок")) ) )
+    val metal
+      = Db.save( Style( Map(en -> Seq("Metal"),
+                            ru -> Seq("Метал")) ) )
+    val grunge
+      = Db.save( Style( Map(en -> Seq("Grunge"),
+                            ru -> Seq("Грандж")) ) )
+
+    Db.save(
+      Artist(
+        names
+          = Map( en -> Seq("Metallica"),
+                 ru -> Seq("Металика", "Металлика") ),
+        styles
+          = Set( metal, rock, hardRock )
+      )
+    )
+    Db.save(
+      Artist(
+        names
+          = Map( en -> Seq("Nirvana"),
+                 ru -> Seq("Нирвана") ),
+        styles
+          = Set( rock, hardRock, grunge )
+      )
+    )
+    Db.save(
+      Artist(
+        names
+          = Map( en -> Seq("Kino"),
+                 ru -> Seq("Кино") ),
+        styles
+          = Set( rock )
+      )
+    )
+    Db.save(
+      Artist(
+        names
+          = Map( en -> Seq("Rolling Stones"),
+                 ru -> Seq("Ролинг Стоунз",
+                           "Роллинг Стоунз",
+                           "Роллинг Стоунс",
+                           "Ролинг Стоунс") ),
+        styles
+          = Set( rock )
+      )
+    )
+    Db.save(
+      Artist(
+        names
+          = Map( en -> Seq("Dire Straits"),
+                 ru -> Seq("Даэр Стрэйтс") ),
+        styles
+          = Set( rock )
+      )
+    )
+  }
+
+
+
+}

da2e4391b07b0596c40a65d5d841135b41f44082
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-12 12:06:46 +0400

    sampledb update

--- src/test/scala/vorm/samples/SampleDb.scala
+++ src/test/scala/vorm/samples/SampleDb.scala
@@ -35,78 +35,54 @@ object SampleDb {
     {
       //  Fill up the db with sample data.
       //  Is placed in the constructor to be executed automatically on initialization of db.
-      //  Is wrapped in a scope to not dispose the local variables to the Db api.
-      {
-        val ru
-          = Db.save( Locale("ru") )
-        val en
-          = Db.save( Locale("en") )
+      //  The value names are purposely not hidden anywhere to be accessible for testing purposes
+      val ru
+        = Db.save( Locale("ru") )
+      val en
+        = Db.save( Locale("en") )
 
-        val rock
-          = Db.save( Style( Map(en -> Seq("Rock"),
-                                ru -> Seq("Рок")) ) )
-        val hardRock
-          = Db.save( Style( Map(en -> Seq("Hard Rock"),
-                                ru -> Seq("Тяжёлый рок", "Тяжелый рок")) ) )
-        val metal
-          = Db.save( Style( Map(en -> Seq("Metal"),
-                                ru -> Seq("Метал")) ) )
-        val grunge
-          = Db.save( Style( Map(en -> Seq("Grunge"),
-                                ru -> Seq("Грандж")) ) )
+      val rock
+        = Db.save( Style( Map( en -> Seq("Rock"),
+                               ru -> Seq("Рок") ) ) )
+      val hardRock
+        = Db.save( Style( Map( en -> Seq("Hard Rock"),
+                               ru -> Seq("Тяжёлый рок", "Тяжелый рок") ) ) )
+      val metal
+        = Db.save( Style( Map( en -> Seq("Metal"),
+                               ru -> Seq("Метал") ) ) )
+      val grunge
+        = Db.save( Style( Map( en -> Seq("Grunge"),
+                               ru -> Seq("Грандж") ) ) )
 
-        Db.save(
-          Artist(
-            names
-              = Map( en -> Seq("Metallica"),
-                     ru -> Seq("Металика", "Металлика") ),
-            styles
-              = Set( metal, rock, hardRock )
-          )
-        )
-        Db.save(
-          Artist(
-            names
-              = Map( en -> Seq("Nirvana"),
-                     ru -> Seq("Нирвана") ),
-            styles
-              = Set( rock, hardRock, grunge )
-          )
-        )
-        Db.save(
-          Artist(
-            names
-              = Map( en -> Seq("Kino"),
-                     ru -> Seq("Кино") ),
-            styles
-              = Set( rock )
-          )
-        )
-        Db.save(
-          Artist(
-            names
-              = Map( en -> Seq("Rolling Stones"),
-                     ru -> Seq("Ролинг Стоунз",
-                               "Роллинг Стоунз",
-                               "Роллинг Стоунс",
-                               "Ролинг Стоунс") ),
-            styles
-              = Set( rock )
-          )
-        )
-        Db.save(
-          Artist(
-            names
-              = Map( en -> Seq("Dire Straits"),
-                     ru -> Seq("Даэр Стрэйтс") ),
-            styles
-              = Set( rock )
-          )
-        )
-        Db.save( Artist( Map( en -> Seq("Godsmack"),
-                              ru -> Seq("Годсмэк") ),
-                         Set( metal, hardRock, rock ) ) )
-      }
+      val metallica
+        = Db.save( Artist( Map( en -> Seq("Metallica"),
+                                ru -> Seq("Металика", "Металлика") ),
+                           Set( metal, rock, hardRock ) ) )
+      val nirvana
+        = Db.save( Artist( Map( en -> Seq("Nirvana"),
+                                ru -> Seq("Нирвана") ),
+                           Set( rock, hardRock, grunge ) ) )
+      val kino
+        = Db.save( Artist( Map( en -> Seq("Kino"),
+                                ru -> Seq("Кино") ),
+                           Set( rock ) ) )
+      val rollingStones
+        = Db.save( Artist( Map( en -> Seq("The Rolling Stones",
+                                          "Rolling Stones",
+                                          "Rolling Stones, The"),
+                                ru -> Seq("Ролинг Стоунз",
+                                          "Роллинг Стоунз",
+                                          "Роллинг Стоунс",
+                                          "Ролинг Стоунс") ),
+                           Set( rock ) ) )
+      val direStraits
+        = Db.save( Artist( Map( en -> Seq("Dire Straits"),
+                                ru -> Seq("Даэр Стрэйтс") ),
+                           Set( rock ) ) )
+      val godsmack
+        = Db.save( Artist( Map( en -> Seq("Godsmack"),
+                                ru -> Seq("Годсмэк") ),
+                           Set( metal, hardRock, rock ) ) )
     }
 
 

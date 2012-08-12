f865db54068bcf430276e0d893f77c79d4015074
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-12 12:27:58 +0400

    formatting correction

--- src/main/scala/vorm/api/Path.scala
+++ src/main/scala/vorm/api/Path.scala
@@ -23,45 +23,44 @@ object Path {
       path : String )
     : Mapping
     = if( path == "" ) parent
-      else
-        try parent match {
-          case parent : EntityMapping =>
-            path.splitBy(".") match {
-              case ("id", remainder) =>
-                remainder match {
-                  case "" =>
-                    parent.id
-                }
-              case (property, remainder) =>
-                mapping(parent.properties(property), remainder)
-            }
-          case parent : TupleMapping =>
-            path.splitBy(".") match {
-              case (item, remainder) =>
-                "(?<=^_)\\d+(?=$)".r.findFirstIn(item) match {
-                  case Some(index) =>
-                    mapping( parent.items(index.toInt - 1), remainder )
-                }
-            }
-          case parent : OptionMapping =>
-            mapping( parent.item, path )
-          case parent : SeqMapping =>
-            path.splitBy(".") match {
-              case ("item", remainder) =>
-                mapping( parent.item, remainder )
-            }
-          case parent : SetMapping =>
-            path.splitBy(".") match {
-              case ("item", remainder) =>
-                mapping( parent.item, remainder )
-            }
-          case parent : MapMapping =>
-            path.splitBy(".") match {
-              case ("key", remainder) =>
-                mapping( parent.key, remainder )
-              case ("value", remainder) =>
-                mapping( parent.value, remainder )
-            }
+      else try parent match {
+        case parent : EntityMapping =>
+          path.splitBy(".") match {
+            case ("id", remainder) =>
+              remainder match {
+                case "" =>
+                  parent.id
+              }
+            case (property, remainder) =>
+              mapping(parent.properties(property), remainder)
+          }
+        case parent : TupleMapping =>
+          path.splitBy(".") match {
+            case (item, remainder) =>
+              "(?<=^_)\\d+(?=$)".r.findFirstIn(item) match {
+                case Some(index) =>
+                  mapping( parent.items(index.toInt - 1), remainder )
+              }
+          }
+        case parent : OptionMapping =>
+          mapping( parent.item, path )
+        case parent : SeqMapping =>
+          path.splitBy(".") match {
+            case ("item", remainder) =>
+              mapping( parent.item, remainder )
+          }
+        case parent : SetMapping =>
+          path.splitBy(".") match {
+            case ("item", remainder) =>
+              mapping( parent.item, remainder )
+          }
+        case parent : MapMapping =>
+          path.splitBy(".") match {
+            case ("key", remainder) =>
+              mapping( parent.key, remainder )
+            case ("value", remainder) =>
+              mapping( parent.value, remainder )
+          }
       } catch {
         case e : MatchError =>
           throw new Exception("Unparseable path `" + path + "` of `" + parent + "`", e)

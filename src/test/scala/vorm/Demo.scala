bcdb811e17659aac989902796e15c1fe555dce39
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-14 03:51:45 +0400

    upd

--- src/test/scala/vorm/Demo.scala
+++ src/test/scala/vorm/Demo.scala
@@ -1,11 +1,13 @@
 package vorm
 
 import api._
+import jdbc._
 import samples._
 import extensions._
 
 import com.codahale.logula.Logging
 import org.apache.log4j.Level
+import java.sql.DriverManager
 
 object Demo extends App {
 
@@ -14,21 +16,9 @@ object Demo extends App {
     log.loggers("vorm.jdbc.ConnectionAdapter") = Level.TRACE
   }
 
-
-  case class A
-    ( a : Option[(Int, Option[String])] )
-
-  val db
-    = new Instance( Entity[A]() :: Nil,
-                    "jdbc:h2:mem:test",
-                    mode = Mode.DropAllCreate )
-
-  db.save(A( None ))
-  db.save(A( Some(2 -> None) ))
-  db.save(A( Some(56 -> Some("asdf")) ))
-
-  db.fetchById[A](1).get.a.trace()
-  db.fetchById[A](2).get.a.trace()
-  db.fetchById[A](3).get.a.trace()
-
+  new ConnectionAdapter(DriverManager.getConnection("jdbc:h2:mem:test"))
+    .executeQuery( Statement("SELECT NOW()") )
+    .parseAndClose()
+    .head.head
+    .trace()
 }

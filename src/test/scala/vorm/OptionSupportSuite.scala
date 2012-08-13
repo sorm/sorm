497124215d58b06ed7af7f1b3e62368fee47f285
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-14 01:48:51 +0400

    clean up

--- src/test/scala/vorm/OptionSupportSuite.scala
+++ src/test/scala/vorm/OptionSupportSuite.scala
@@ -29,37 +29,11 @@ class OptionSupportSuite extends FunSuite with ShouldMatchers {
       } should produce [IllegalArgumentException]
   }
 
-  test("Value property in Option"){
-    val db
-      = new Instance( Entity[EntityWithValuePropertyInOption]() :: Nil,
-                      "jdbc:h2:mem:test",
-                      mode = Mode.DropAllCreate
-                      )
-    db.save(EntityWithValuePropertyInOption(None))
-    db.save(EntityWithValuePropertyInOption(Some(3)))
-    db.save(EntityWithValuePropertyInOption(Some(7)))
-
-    db.fetchById[EntityWithValuePropertyInOption](1).get.a === None
-    db.fetchById[EntityWithValuePropertyInOption](2).get.a === Some(3)
-
-    db.query[EntityWithValuePropertyInOption]
-      .filterEquals("a", None).fetchOne().get.id === 1
-    db.query[EntityWithValuePropertyInOption]
-      .filterEquals("a", Some(3)).fetchOne().get.id === 2
-  }
-
-  test("Seq in Option")(pending)
-  test("Map key in Option")(pending)
-  test("Map value in Option")(pending)
-  test("Seq item in Option")(pending)
-  test("Set item in Option")(pending)
-
 }
 object OptionSupportSuite {
 
   case class EntityWithOptionInOption
     ( optionInOption : Option[Option[Int]] )
-  case class EntityWithValuePropertyInOption
-    ( a : Option[Int] )
+
 }
 

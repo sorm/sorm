e256c0b2c71c4b7d582e101a754d079ca8c199d8
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-13 18:47:02 +0400

    empty spec and suite

---
+++ src/test/scala/vorm/DateTimeSupportSuite.scala
@@ -0,0 +1,28 @@
+package vorm
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import vorm._
+import dropAll._
+import persisted._
+import query._
+import reflection._
+import save._
+import structure._
+import mapping._
+import jdbc._
+import create._
+import drop._
+import extensions._
+
+import samples._
+
+@RunWith(classOf[JUnitRunner])
+class DateTimeSupportSuite extends FunSuite with ShouldMatchers {
+
+  import SampleDb._
+
+}

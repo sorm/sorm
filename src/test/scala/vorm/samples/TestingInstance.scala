b8f4278148f6ea8cfc837f4e61cb4cea6207059a
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-18 01:00:12 +0400

    simulator testing instance

--- src/test/scala/vorm/samples/TestingInstance.scala
+++ src/test/scala/vorm/samples/TestingInstance.scala
@@ -4,9 +4,30 @@ import vorm._
 import api._
 import jdbc._
 import samples._
+import save._
+import structure._
+import mapping._
 import extensions._
 
 object TestingInstance {
+
+  def simulator
+    ( entities : Entity[_]* )
+    = new Api {
+        protected[vorm] val connection
+          = new ConnectionAdapterSimulator()
+              with SaveAdapter
+
+        protected[vorm] val mappings
+          = {
+            val settings
+              = entities.view.map{ e => e.reflection -> e.settings }.toMap
+
+            settings.keys
+              .zipBy{ new EntityMapping(None, _, settings) }
+              .toMap
+          }
+      }
   def h2
     ( entities : Entity[_]* )
     = new Instance( entities, "jdbc:h2:mem:test", mode = Mode.DropAllCreate )

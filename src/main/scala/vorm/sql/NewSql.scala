1893051dcd4d24d760950038a830800abcca6d07
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-22 01:00:22 +0400

    match-based

--- src/main/scala/vorm/sql/NewSql.scala
+++ src/main/scala/vorm/sql/NewSql.scala
@@ -10,19 +10,80 @@ object NewSql {
   sealed trait Statement 
     extends Sql
 
-  sealed case class Union 
-    [ L <: Statement,
-      R <: Statement ]
-    ( left : L,  
-      right : R ) 
+  case class Union 
+    ( left  : Statement,  
+      right : Statement ) 
     extends Statement
+    with FromObject 
+    with JoinObject
 
-//  sealed case class Select
-//    ( what : Seq[SelectWhat],
-//      from : From,
-//      join : Seq[Join] = Nil,
-//      where : Option[Clause] )
-//    extends Statement
+  case class Select
+    ( what : Seq[WhatObject],
+      from : From,
+      join : Seq[Join] = Nil,
+      where : Option[Clause] = None,
+      groupBy : Seq[GroupByObject] = Nil,
+      having : Option[Clause] = None,
+      orderBy : Seq[OrderByClause] = Nil,
+      limit : Option[Int] = None,
+      offset : Option[Int] = None )
+    extends Statement
+    with FromObject 
+    with JoinObject
+
+  sealed trait WhatObject extends Renderable
+
+  sealed trait GroupByObject extends Renderable
+
+  case class OrderByClause
+    ( what : Column,
+      desc : Boolean = false )
+    extends Renderable
+
+
+  case class Table
+    ( name : String )
+    extends FromObject 
+    with JoinObject
+
+  case class From
+    ( what : FromObject,
+      as : Option[String] = None )
+
+  sealed trait FromObject extends Renderable
+
+  case class Join
+    ( what : JoinObject,
+      as : Option[String] = None,
+      on : Seq[(Column, Column)] = Nil,
+      kind : JoinKind = JoinKind.Left )
+
+  sealed trait JoinObject extends Renderable
+
+  sealed trait JoinKind
+  object JoinKind {
+    case object Left  extends JoinKind
+    case object Right extends JoinKind
+    case object Inner extends JoinKind
+  }
+
+  case class Column
+    ( name : String,
+      table : Option[String] = None )
+    extends SelectObject 
+    with ConditionObject
+    with GroupByObject
+
+  case class Count
+    ( what : Seq[Column],
+      distinct : Boolean = false )
+    extends SelectObject 
+    with ConditionObject
+    with GroupByObject
+
+
+  trait Clause extends Renderable
+  trait ConditionObject extends Renderable
 
 
 }
--- src/main/scala/vorm/sql/StandardRendering.scala
+++ src/main/scala/vorm/sql/StandardRendering.scala
@@ -12,20 +12,65 @@ object StandardRendering extends RenderingSyntax {
     ( s : String )
     = s
 
-  implicit def unionRendering
-    [ L <: Statement : Rendering,
-      R <: Statement : Rendering ]
-    = new Rendering[Union[L, R]] {
+  implicit def sqlRendering
+    = new Rendering[Sql] {
         def template 
-          ( self : Union[L, R] )
-          = "( " + implicitly[Rendering[L]].template(self.left).indent(2).trim + " )\n" +
-            "UNION\n" +
-            "( " + implicitly[Rendering[R]].template(self.right).indent(2).trim + " )\n" 
+          ( self : Sql )
+          = self match {
+              case Union(l, r) =>
+                "( " + l.rendering.indent(2).trim + " )\n" +
+                "UNION\n" +
+                "( " + r.rendering.indent(2).trim + " )\n"
+              case Select(what, from, join, where, groupBy, having, orderBy, limit, offset) =>
+                "SELECT\n" +
+                ( what.view.map{_.rendering}.mkString(", ") +
+                  "\n" + from.rendering +
+                  join
+                    .view
+                    .map{ _.rendering }
+                    .mkString("\n")
+                    .satisfying{ ! _.isEmpty }
+                    .map{"\n" + _}
+                    .getOrElse("") +
+                  where
+                    .map{ _.rendering }
+                    .map{ "\nWHERE " + _.indent("WHERE ".length).trim }
+                    .getOrElse("") +
+                  groupBy
+                    .view
+                    .map{ _.rendering }
+                    .mkString(", ")
+                    .satisfying{ ! _.isEmpty }
+                    .map{ "\nGROUP BY " + _ }
+                    .getOrElse("") +
+                  having
+                    .map{ _.rendering }
+                    .map{ "\nHAVING " + _.indent("HAVING ".length).trim }
+                    .getOrElse("") +
+                  orderBy
+                    .map{ _.rendering }
+                    .mkString(", ")
+                    .satisfying{ ! _.isEmpty }
+                    .map{ "\nORDER BY " + _ }
+                    .getOrElse("") +
+                  limit
+                    .map{ "\nLIMIT " + _ }
+                    .getOrElse("") +
+                  offset
+                    .map{ "\nOFFSET " + _ }
+                    .getOrElse("") )
+                  .indent(2)
+            }
         def data
-          ( self : Union[L, R] )
-          = implicitly[Rendering[L]].data(self.left) ++: 
-            implicitly[Rendering[R]].data(self.right) ++: 
-            Stream()
+          ( self : Sql )
+          = self match {
+              case Union(l, r) =>
+                l.data ++: r.data ++: Stream()
+              case Select(what, from, join, where, groupBy, having, orderBy, limit, offset) =>
+                ( what ++: from +: join ++: where ++: groupBy ++: having ++:
+                  orderBy ++: Nil )
+                  .flatMap{_.data}
+            }
       }
 
 }

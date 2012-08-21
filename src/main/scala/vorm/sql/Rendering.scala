03906e4c40c409616b066713ebb1d79bad2b80bb
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-22 00:21:09 +0400

    init

--- src/main/scala/vorm/sql/Rendering.scala
+++ src/main/scala/vorm/sql/Rendering.scala
@@ -7,26 +7,9 @@ import NewSql._
 
 object Rendering {
 
-  trait Rendering[T <: Sql] {
-    def template ( self : T ) : String
-    def data ( self : T ) : Seq[Any]
+  trait Rendering {
+    def template : String
+    def data : Seq[Any]
   }
-
-  trait RenderingSyntax {
-    implicit class SqlExtensions 
-      [ T <: Sql : Rendering ] 
-      ( self : T ) 
-      {
-        def template
-          = implicitly[Rendering[T]].template(self)
-        def data
-          = implicitly[Rendering[T]].data(self)
-      }
-  }
-
-  // case class Rendering
-  //   ( template : String, 
-  //     data : Seq[Any] )
-
 
 }
--- src/main/scala/vorm/sql/StandardRendering.scala
+++ src/main/scala/vorm/sql/StandardRendering.scala
@@ -6,26 +6,25 @@ import extensions._
 import NewSql._
 import Rendering._
 
-object StandardRendering extends RenderingSyntax {
+object StandardRendering {
 
   private def quote
     ( s : String )
     = s
 
-  implicit def unionRendering
-    [ L <: Statement : Rendering,
-      R <: Statement : Rendering ]
-    = new Rendering[Union[L, R]] {
-        def template 
-          ( self : Union[L, R] )
-          = "( " + implicitly[Rendering[L]].template(self.left).indent(2).trim + " )\n" +
-            "UNION\n" +
-            "( " + implicitly[Rendering[R]].template(self.right).indent(2).trim + " )\n" 
-        def data
-          ( self : Union[L, R] )
-          = implicitly[Rendering[L]].data(self.left) ++: 
-            implicitly[Rendering[R]].data(self.right) ++: 
-            Stream()
-      }
+  implicit class UnionRendering
+    [ L <: Statement <% Rendering,
+      R <: Statement <% Rendering ]
+    ( self : Union[L, R] )
+    {
+      def template 
+        ( self : Union[L, R] )
+        = "( " + self.left.template.indent(2).trim + " )\n" +
+          "UNION\n" +
+          "( " + self.right.template.indent(2).trim + " )\n" 
+      def data
+        ( self : Union[L, R] )
+        = self.left.data ++: self.right.data ++: Stream()
+    }
 
 }

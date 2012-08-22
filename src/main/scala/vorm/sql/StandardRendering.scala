f26039dca002de2c3a292658d73598b6efba75f2
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-22 22:34:25 +0400

    upd

--- src/main/scala/vorm/sql/StandardRendering.scala
+++ src/main/scala/vorm/sql/StandardRendering.scala
@@ -21,7 +21,7 @@ object StandardRendering extends RendererSyntax {
                 "( " + template(l).indent(2).trim + " )\n" +
                 "UNION\n" +
                 "( " + template(r).indent(2).trim + " )\n"
-              case Select(what, from, join, where, groupBy, having, orderBy, limit, offset) =>
+              case Select(what, from, join, where, groupBy, having, orderBy,  limit, offset) =>
                 "SELECT\n" +
                 ( what.view.map{template}.mkString(", ") +
                   "\n" + template(from) +
@@ -60,17 +60,125 @@ object StandardRendering extends RendererSyntax {
                     .map{ "\nOFFSET " + _ }
                     .getOrElse("") )
                   .indent(2)
+              case OrderBy(what, desc) =>
+                template(what) + ( if( desc ) " DESC" else "" )
+              case Table(name) =>
+                quote(name)
+              case From(what, as) =>
+                "FROM\n" +
+                ( ( what match {
+                      case Table(name) ⇒ quote(name)
+                      case s : Sql ⇒ "(\n" + template(s).indent(2) + "\n)"
+                    } ) +
+                  as.map{ "\nAS " + quote(_) }.getOrElse("") 
+                  )
+                  .indent(2)
+              case Join(what, as, on, kind) =>
+                ( kind match {
+                    case JoinKind.Left ⇒ "LEFT JOIN "
+                    case JoinKind.Right ⇒ "RIGHT JOIN "
+                    case JoinKind.Inner ⇒ "INNER JOIN "
+                  } ) + "\n" +
+                ( ( what match {
+                      case Table(name) ⇒ quote(name)
+                      case s : Sql ⇒ "( " + template(s).indent(2).trim + " )"
+                    } ) +
+                  as.map{ "\nAS " + quote(_) }
+                    .getOrElse("") +
+                  on.map{ case (l, r) ⇒ template(l) + " = " + template(r) }
+                    .mkString(" AND\n")
+                    .satisfying{ ! _.isEmpty }
+                    .map{ "\nON " + _.indent("ON ".length).trim }
+                    .getOrElse("")
+                  )
+                  .indent(2)
+              case Column(name, table) =>
+                table.map{quote(_) + "."}.getOrElse("") + quote(name)
+              case Count(what, distinct) =>
+                "COUNT(" +
+                ( if( distinct ) "DISTINCT " else "" ) +
+                template(what) +
+                ")"
+              case Value(_) =>
+                "?"
+              case CompositeCondition(l, r, o) =>
+                def subConditionSql
+                  ( c : Condition[_] )
+                  : String
+                  = c match {
+                      case c : CompositeCondition[_] =>
+                        "( " + template(c).indent("( ".length).trim + " )"
+                      case c => 
+                        template(c)
+                    }
+                subConditionSql(l) + " " + 
+                template(o) + "\n" + 
+                subConditionSql(r)
+              case IsNull(what, negative) =>
+                template(what) + 
+                " IS" + ( if( negative ) " NOT" else "" ) + " NULL"
+              case Comparison(l, r, o) =>
+                template(l) + " " + template(o) + " " + template(r)
+              case And => 
+                "AND"
+              case Or => 
+                "OR"
+              case Equal => 
+                "="
+              case NotEqual => 
+                "<>"
+              case Larger => 
+                ">"
+              case LargerOrEqual => 
+                ">="
+              case Smaller => 
+                "<"
+              case SmallerOrEqual => 
+                "<="
+              case Like => 
+                "LIKE"
+              case NotLike => 
+                "NOT LIKE"
+              case Regexp => 
+                "REGEXP"
+              case NotRegexp => 
+                "NOT REGEXP"
+              case In => 
+                "IN"
+              case NotIn => 
+                "NOT IN"
             }
 
         def data
           ( self : Sql )
           = self match {
-              case Union(l, r) =>
-                data(l) ++: data(r) ++: Stream()
+              case Union(l, r) => 
+                data(l) ++: data(r)
               case Select(what, from, join, where, groupBy, having, orderBy, limit, offset) =>
                 ( what ++: from +: join ++: where ++: groupBy ++: having ++:
                   orderBy ++: Nil )
                   .flatMap{data}
+              case OrderBy(what, desc) => 
+                data(what)
+              case Table(_) => 
+                Stream()
+              case From(what, as) =>
+                data(what)
+              case Join(what, _, _, _) =>
+                data(what)
+              case Column(_, _) =>
+                Stream()
+              case Count(_, _) =>
+                Stream()
+              case Value(v) =>
+                Stream(v)
+              case CompositeCondition(l, r, _) =>
+                data(l) ++: data(r)
+              case IsNull(_, _) =>
+                Stream()
+              case Comparison(l, r, _) =>
+                data(l) ++: data(r)
+
             }
       }
 

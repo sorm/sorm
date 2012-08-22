1ed35a6bf63b162e9198a8fc964aaeb2a549df2a
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-22 19:16:27 +0400

    upd

--- src/main/scala/vorm/sql/StandardRendering.scala
+++ src/main/scala/vorm/sql/StandardRendering.scala
@@ -3,7 +3,7 @@ package vorm.sql
 import vorm._
 import extensions._
 
-import NewSql._
+import Sql._
 import Rendering._
 
 object StandardRendering {
@@ -14,14 +14,15 @@ object StandardRendering {
 
   implicit class RenderableSql
     ( self : Sql )
+    extends Renderable
     {
-      def template 
+      def template
         = self match {
             case Union(l, r) =>
               "( " + l.template.indent(2).trim + " )\n" +
               "UNION\n" +
-              "( " + r.template.indent(2).trim + " )\n" 
-            case Select(what, from, join, where, groupBy, having, orderBy, 
+              "( " + r.template.indent(2).trim + " )\n"
+            case Select(what, from, join, where, groupBy, having, orderBy,
                         limit, offset) =>
               ???
           }

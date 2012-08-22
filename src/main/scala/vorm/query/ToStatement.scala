a225eec3c6baa070e0e976f75119014c196e310a
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-22 14:45:01 +0400

    init

---
+++ src/main/scala/vorm/query/ToStatement.scala
@@ -0,0 +1,33 @@
+package vorm.query
+
+import vorm._
+import reflection._
+import structure._
+import mapping._
+import jdbc._
+import extensions._
+
+object ToStatement {
+
+  // implicit class QueryToStatement ( self : Query ) {
+  //   def toStatement
+  //     = self.toSql.toStatement
+  // }
+
+  // implicit class QueryToSql ( self : Query ) {
+  //   def toSql
+  //     = self
+  //         .
+  // }
+
+  // implicit class QueryIdSql ( self : Query ) {
+  //   def idSql
+  //     = self.where.sql
+  // }
+
+  implicit class WhereIdSql ( self : Where ) {
+    def idSql
+      = ???
+  }
+
+}

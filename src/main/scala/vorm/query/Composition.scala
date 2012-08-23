ec0c3193ec2be8bdbba591bfc1a7e57459c70918
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-23 15:23:05 +0400

    new clauses

--- src/main/scala/vorm/query/Composition.scala
+++ src/main/scala/vorm/query/Composition.scala
@@ -1,51 +1,64 @@
 package vorm.query
 
-import vorm._
-import reflection._
-import structure._
-import mapping._
-import persisted._
-import abstractSql._
-import extensions._
+import vorm.reflection._
+import vorm.structure._
+import vorm.structure.mapping._
+import vorm.persisted._
+import vorm.extensions._
 
 import Query._
 import Operator._
-import abstractSql.Composition._
+import vorm.abstractSql.{AbstractSql => AS}
+import vorm.abstractSql.Composition._
 
 object Composition {
 
   def rootKeyStatement
     ( query : Query )
-    : AbstractSql.Statement
+    : AS.Statement
     = ???
 
   def rootKeyStatement
     ( where : Where )
-    : AbstractSql.Statement
+    : AS.Statement
     = where match {
+        case Filter(Equals, m : ValueMapping, v) =>
+          m.root.abstractSqlPrimaryKeySelect
+            .copy(
+              condition
+                = Some(
+                    AS.Comparison(
+                      m.containerTableMapping.get.abstractSqlTable,
+                      m.columnName,
+                      AS.Equal,
+                      v
+                    )
+                  )
+            )
         case Filter(Equals, m : SeqMapping, v : Seq[_]) =>
-          intersection(
-            v.view
-              .zipWithIndex
-              .map{ case (v, i) =>
-                intersection(
-                  rootKeyStatement(Filter(Equals, m.index, i)),
-                  rootKeyStatement(Filter(Equals, m.item, v))
+          val matching
+            = v.view
+                .zipWithIndex
+                .map{ case (v, i) =>
+                  intersection(
+                    rootKeyStatement(Filter(Equals, m.index, i)),
+                    rootKeyStatement(Filter(Equals, m.item, v))
+                  )
+                }
+                .reduceOption{union}
+                .map{
+                  select(_).copy(
+                    havingCount
+                      = Some(AS.HavingCount(m.abstractSqlTable, v.size))
+                  )
+                }
+          val size
+            = m.root.abstractSqlPrimaryKeySelect
+                .copy(
+                  havingCount
+                    = Some(AS.HavingCount(m.abstractSqlTable, v.size))
                 )
-              }
-              .reduceOption{union}
-              .map{select}
-              .getOrElse( m.root.abstractSqlPrimaryKeySelect )
-              .copy(
-                havingCount
-                  = Some(AbstractSql.HavingCount(m.abstractSqlTable, v.size))
-              ),
-            m.root.abstractSqlPrimaryKeySelect
-              .copy(
-                havingCount
-                  = Some(AbstractSql.HavingCount(m.abstractSqlTable, v.size))
-              )
-          )
+          matching.foldLeft(size : AS.Statement){AS.Intersection}
       }
 
 }

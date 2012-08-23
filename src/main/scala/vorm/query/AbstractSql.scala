f5e0ac024a989df14b185bbf903db461458b24a1
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-23 05:09:40 +0400

    AbstractSql!

---
+++ src/main/scala/vorm/query/AbstractSql.scala
@@ -0,0 +1,71 @@
+package vorm.query
+
+import vorm._
+import reflection._
+import structure._
+import mapping._
+import persisted._
+import sql._
+import extensions._
+
+object AbstractSql {
+
+  
+  trait Statement
+
+  case class Union
+    ( left : Statement, right : Statement )
+    extends Statement
+
+  case class Intersection
+    ( left : Statement, right : Statement )
+    extends Statement
+
+  case class Select
+    ( expressions : Seq[Expression],
+      condition : Option[Condition] = None,
+      havingRows : Option[HavingCount] = None )
+    extends Statement
+
+
+  case class HavingCount
+    ( table : Table,
+      count : Int )
+
+
+  trait Expression
+
+  case class Column
+    ( name : String,
+      table : Table )
+
+
+  trait Condition
+
+  case class And
+    ( left : Condition, right : Condition )
+    extends Condition
+  
+  case class Or
+    ( left : Condition, right : Condition )
+    extends Condition
+
+  case class Comparison
+    ( table : Table,
+      column : String,
+      operator : Sql.ComparisonOperator,
+      value : Any )
+    extends Condition
+
+
+  case class Table
+    ( name : String,
+      parent : Option[Parent] )
+
+  case class Parent
+    ( table : Table,
+      bindings : Seq[(String, String)] )
+  
+
+
+}

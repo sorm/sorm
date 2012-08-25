f3175d791051b263a35336d9fce696e875a20540
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-25 15:12:28 +0400

    upd

--- src/main/scala/sorm/reflection/Api.scala
+++ src/main/scala/sorm/reflection/Api.scala
@@ -4,21 +4,22 @@ import reflect.runtime.universe._
 import reflect.runtime.{currentMirror => mirror}
 import sorm.extensions.Extensions._
 import ScalaApi._
+
 trait Api {
 
   def t : Type
 
   def properties
-     = t.properties
-         .map{ s => s.decodedName -> Reflection(s.typeSignature) }
-         .toMap
+    = t.properties
+        .map{ s => s.decodedName -> Reflection(s.typeSignature) }
+        .toMap
   def generics
-     = t match {
-         case t : TypeRef => t.args.toStream.map{ Reflection(_) }
-       }
+    = t match {
+        case t : TypeRef => t.args.toStream.map{ Reflection(_) }
+      }
   def signature : String
-     = if( generics.isEmpty ) t.fullName
-       else t.fullName + "[" + generics.map(_.signature).mkString(", ") + "]"
+    = if( generics.isEmpty ) t.fullName
+      else t.fullName + "[" + generics.map(_.signature).mkString(", ") + "]"
 
   override def toString = t.toString
 

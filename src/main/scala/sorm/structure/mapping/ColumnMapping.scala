7cf803aa8592a3f7253c9370dbfd9c5feb3c14ef
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-26 20:59:21 +0400

    clean nullable impl

--- src/main/scala/sorm/structure/mapping/ColumnMapping.scala
+++ src/main/scala/sorm/structure/mapping/ColumnMapping.scala
@@ -39,56 +39,10 @@ trait ColumnMapping
     def autoIncremented : Boolean
     
     lazy val nullable
-      = {
-        // def ancestors 
-        //   = unfold1 { _.parent }
-        // // def ancestors = unfold { _.parent map { p => p -> p } }
-        // // def ancestors = parent unfold { p => p -> p flatMap {_.parent} }
-
-        // def ancestorsTillTable 
-        //   = ancestors takeWhile {!_.isInstanceOf[TableMapping]}
-
-
-        // parent unfold {
-        //   _ flatMap {
-        //     case m : TableMapping => Some(m, None)
-        //     case m => Some(m, m.parent)
-        //   }
-        // }
-        // .exists{_.isInstanceOf[OptionMapping]}
-
-        // parent unfold {
-        //   case None => None
-        //   case Some(m : TableMapping) => Some(m, None)
-        //   case Some(m) => Some(m, m.parent)
-        // }
-
-        // unfold {
-        //   case m : TableMapping => None
-        //   case m => Some(m, m.parent)
-        // }
-
-
-
-        // def ancestorsToTable
-        //   ( m : Mapping )
-        //   : Stream[Mapping]
-        //   = m.parent.toStream flatMap {
-        //       case p : TableMapping => Stream(p)
-        //       case p => p #:: ancestorsToTable(p)
-        //     }
-
-        // ancestorsToTable(this)
-        //   .exists{_.isInstanceOf[OptionMapping]}
-        
-        // parent.iterate( _.flatMap(_.parent) )
-        
-        ( this : Mapping )
+      = ( this : Mapping )
           .unfold1( _.parent )
           .takeWhile( !_.isInstanceOf[TableMapping] )
           .exists( _.isInstanceOf[OptionMapping] )
-
-      }
 
     lazy val column
       = Column(columnName, columnType, autoIncremented, nullable)

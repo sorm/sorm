a297a24818a631d0f213345264762beb6cb777a9
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-08-27 20:26:10 +0400

    toInstanceOf tests

--- src/test/scala/sorm/extensions/ExtensionsTest.scala
+++ src/test/scala/sorm/extensions/ExtensionsTest.scala
@@ -8,17 +8,25 @@ import sorm.extensions.Extensions._
 
 @RunWith(classOf[JUnitRunner])
 class ExtensionsTest extends FunSuite with ShouldMatchers {
+  import ExtensionsTest._
   test("mapKeys") {
     Map("b" -> 1, "c" -> 4, "a" -> 9).mapKeys(_ + "1") should
     equal(Map("b1" -> 1, "c1" -> 4, "a1" -> 9))
   }
-  test("asInstanceOfOption returns None for unmatching type"){
+  test("toInstanceOf returns None for unmatching type"){
     8.toInstanceOf[String] should equal(None)
   }
-  test("asInstanceOfOption returns Some for a matching type"){
+  test("toInstanceOf returns Some for a matching type"){
     8.toInstanceOf[Int] should equal(Some(8))
   }
-  test("asInstanceOfOption returns Some for an inheriting type"){
+  test("toInstanceOf returns Some for an inheriting type"){
     List(1).toInstanceOf[Seq[Int]] should be ('defined)
   }
+  test("toInstanceOf on mixins"){
+    (new A with B).toInstanceOf[B] should be ('defined)
+  }
+}
+object ExtensionsTest {
+  trait A
+  trait B
 }

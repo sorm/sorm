fc5ca9c793d9f1a6e93fa4df1b6fd2fe540e88b0
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-11 18:26:03 +0400

    date larger test

--- src/test/scala/sorm/DateTimeSupportSuite.scala
+++ src/test/scala/sorm/DateTimeSupportSuite.scala
@@ -5,30 +5,38 @@ import org.scalatest.matchers.ShouldMatchers
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-import sorm._
-import dropAll._
-import persisted._
-import query._
-import reflection._
-import save._
-import structure._
-import mapping._
-import jdbc._
-import create._
-import drop._
 import sext.Sext._
 
 import samples._
+import org.joda.time.DateTime
 
 @RunWith(classOf[JUnitRunner])
 class DateTimeSupportSuite extends FunSuite with ShouldMatchers {
+  import sorm.DateTimeSupportSuite._
+
+  val db = TestingInstance.h2(Sorm.Entity[A]())
+
+  val date = new DateTime()
+
+  val a1 = db.save(A(date))
+  val a2 = db.save(A(date.plusHours(3)))
+  val a3 = db.save(A(date.minusSeconds(5)))
+  val a4 = db.save(A(date.minusSeconds(50)))
 
 
   test("Api fetchDate")(pending)
-  test("Bigger filter")(pending)
+  test("Larger filter"){
+    db.all[A].filterLarger("a", date.minusSeconds(1)).fetch()
+      .should(
+        contain(a1) and contain(a2) and not contain(a3) and not contain(a4)
+      )
+  }
   test("Smaller filter")(pending)
   test("Equal filter")(pending)
   test("In filter")(pending)
   test("Other filters fail")(pending)
 
+}
+object DateTimeSupportSuite {
+  case class A ( a : DateTime )
 }

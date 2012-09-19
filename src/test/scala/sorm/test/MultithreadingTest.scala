802b6af0de4a559bc463d1e1d5d56d741e6970ae
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-19 15:59:13 +0400

    multithreadingtest

---
+++ src/test/scala/sorm/test/MultithreadingTest.scala
@@ -0,0 +1,31 @@
+package sorm.test
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import sorm.samples.TestingInstance
+import sorm.Sorm.Entity
+
+object MultithreadingTest {
+  case class A (a : Int)
+}
+@RunWith(classOf[JUnitRunner])
+class MultithreadingTest extends FunSuite with ShouldMatchers {
+  import DeadlockTest._
+
+  val db = TestingInstance.mysql(Entity[A](unique = Set() + Seq("a")))
+
+  val a1 = db.save(A(1))
+  val a2 = db.save(A(3))
+  val a3 = db.save(A(0))
+  val a4 = db.save(A(3000))
+
+  test("Parallel queries"){
+    Seq(0,1,2,3).par.flatMap{ i => db.one[A].filterEqual("a", i).fetch() }.seq
+      .should(contain(a1) and contain(a3) and not contain(a4))
+  }
+  test("Parallel saving"){
+    pending
+  }
+}

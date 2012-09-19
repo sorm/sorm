7f49323d136715a30b0a1d61a7188e454b66e8f2
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-19 20:38:26 +0400

    deadlock

--- src/main/scala/sorm/jdbc/Transactions.scala
+++ src/main/scala/sorm/jdbc/Transactions.scala
@@ -9,44 +9,32 @@ trait Transactions {
   protected def connection : Connection
   private var thread : Thread = _
   def transaction [ T ] ( t : => T ) : T
+    = transaction(100)(t)
+
+  private def transaction [ T ] ( attempts : Int ) ( t : => T ) : T
     = synchronized {
         if( !connection.getAutoCommit ){
           assume( Thread.currentThread == thread, "Attempt to get a transaction started on a connection which is already in one but on a different thread" )
           t
         } else {
-          var committed = false
           try {
-//            retry[SQLTransactionRollbackException, T](50, 1){
-//              connection.setAutoCommit(false)
-//              thread = Thread.currentThread
-//              val r = t
-//              connection.commit()
-//              committed = true
-//              r
-//            }
             connection.setAutoCommit(false)
             thread = Thread.currentThread
             val r = t
             connection.commit()
-            committed = true
-            r
-          } finally {
-            if( !committed ) connection.rollback()
             connection.setAutoCommit(true)
+            r
+          } catch {
+            case e : SQLTransactionRollbackException if attempts > 0 =>
+              connection.rollback()
+              connection.setAutoCommit(true)
+              transaction(attempts - 1)(t)
+            case e : Throwable =>
+              connection.rollback()
+              connection.setAutoCommit(true)
+              throw e
           }
         }
-      }
-
-
-  private def retry [ E <: Throwable : ClassTag, T ] ( times : Int, interval : Long = 0 ) ( f : => T ) : T
-    = try f
-      catch {
-        case e : E =>
-          if( times > 0 ) {
-            if( interval > 0l ) Thread.sleep(interval)
-            retry(times - 1, interval)(f)
-          }
-          else throw e
       }
 
 }

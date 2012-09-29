cf0a9e1aabf54bf7f6d960c9a79badd2f2ce7779
Nikita Volkov <justnika@mail.ru>
2012-09-30 02:02:13 +0400

    driver upd

--- src/main/scala/sorm/core/Driver.scala
+++ src/main/scala/sorm/core/Driver.scala
@@ -5,7 +5,15 @@ import sext.Sext._
 import sorm._
 import abstractSql._
 import abstractSql.AbstractSql._
+import jdbc.JdbcValue
+import sql._
 
 trait Driver {
   def statement ( asql : Statement ) : jdbc.Statement
+    = asql as sql as statement
+  def statement ( sql : Sql.Sql ) : jdbc.Statement
+    = jdbc.Statement(template(sql), data(sql).map(JdbcValue.apply))
+  def sql ( asql : Statement ) : Sql.Statement
+  def template ( sql : Sql.Sql ) : String
+  def data ( sql : Sql.Sql ) : Seq[Any]
 }

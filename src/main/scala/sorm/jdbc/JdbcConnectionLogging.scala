b765c9733070a76077b28762447a2530ff5319cf
Nikita Volkov <justnika@mail.ru>
2012-10-25 19:26:36 +0400

    ns-based benchmarking

--- src/main/scala/sorm/jdbc/JdbcConnectionLogging.scala
+++ src/main/scala/sorm/jdbc/JdbcConnectionLogging.scala
@@ -7,9 +7,9 @@ import org.joda.time.Period
 trait JdbcConnectionLogging {
   private lazy val slf4j : org.slf4j.Logger = LoggerFactory getLogger this.getClass()
   private def logging = slf4j.isDebugEnabled
-  private def log (s : Statement, time : Period) {
+  private def log (s : Statement, ns : Long) {
     slf4j.debug(
-      "Executed statement in " + time + ":\n" +
+      "Executed statement in %,.3fms:\n".format(ns / 1000000d) +
       (("sql" -> s.sql) +: s.data.map(_.value).notEmpty.map("data" -> _) ++: Stream())
         .toMap.valueTreeString
     )

55d428cc34ffe9245729399aa28f4ec73866b079
Nikita Volkov <justnika@mail.ru>
2012-11-06 09:58:13 +0400

    connection pool logging

--- src/main/scala/sorm/pooling/ConnectionPool.scala
+++ src/main/scala/sorm/pooling/ConnectionPool.scala
@@ -1,8 +1,9 @@
 package sorm.pooling
 
 import sorm.jdbc.JdbcConnection
+import com.weiglewilczek.slf4s.Logging
 
-trait ConnectionPool {
+trait ConnectionPool extends Logging {
   protected def fetchConnection () : JdbcConnection
   protected def returnConnection ( c : JdbcConnection )
 
@@ -11,14 +12,17 @@ trait ConnectionPool {
   def withConnection [ T ] ( f : JdbcConnection => T ) : T
     = {
       val thread = Thread.currentThread()
+      logger.trace("Opening a withConnection block on thread " + thread)
       openConnectionByThread.get(thread) match {
         case Some(cx) =>
           f(cx)
         case None =>
+          logger.trace("Fetching a connection to thread " + thread)
           val cx = fetchConnection()
           openConnectionByThread.update(thread, cx)
           try f(cx)
           finally {
+            logger.trace("Returning a connection from thread " + thread)
             openConnectionByThread.remove(thread)
             returnConnection(cx)
           }

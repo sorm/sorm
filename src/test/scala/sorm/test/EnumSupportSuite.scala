48eba45296c219ba67f63f632e8627de6e783f0b
Nikita Volkov <justnika@mail.ru>
2012-11-07 01:20:37 +0400

    std multi-db enum suite

--- src/test/scala/sorm/test/EnumSupportSuite.scala
+++ src/test/scala/sorm/test/EnumSupportSuite.scala
@@ -15,27 +15,28 @@ import samples._
 class EnumSupportSuite extends FunSuite with ShouldMatchers {
   import EnumSupportSuite._
 
-  val db = TestingInstance.mysql( Entity[A]() )
+  TestingInstance.all(Entity[A]()).foreach(test)
 
-  val a1 = db.save(A(B.One))
-  val a2 = db.save(A(B.Two))
-  val a3 = db.save(A(B.Three))
-  val a4 = db.save(A(B.Two))
-
-  test("Equality query"){
-    db.access[A].whereEqual("a", B.Two).fetch()
-      .should(
-        have size (2) and
-        contain (a2)
-      )
-  }
-  test("Not equals query"){
-    db.access[A].whereNotEqual("a", B.Two).fetch()
-      .should(
-        not be 'empty and
-        contain (a1) and
-        contain (a3)
-      )
+  def test ( db : Instance ) {
+    val a1 = db.save(A(B.One))
+    val a2 = db.save(A(B.Two))
+    val a3 = db.save(A(B.Three))
+    val a4 = db.save(A(B.Two))
+    test("Equality query - " + db){
+      db.access[A].whereEqual("a", B.Two).fetch()
+        .should(
+          have size (2) and
+          contain (a2)
+        )
+    }
+    test("Not equal query - " + db){
+      db.access[A].whereNotEqual("a", B.Two).fetch()
+        .should(
+          not be 'empty and
+          contain (a1) and
+          contain (a3)
+        )
+    }
   }
 
 }

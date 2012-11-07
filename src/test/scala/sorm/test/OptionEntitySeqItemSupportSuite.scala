5f6a9316a4b1d5268c6bd65d57ac49601e73bf43
Nikita Volkov <justnika@mail.ru>
2012-11-07 16:41:02 +0400

    test fix

--- src/test/scala/sorm/test/OptionEntitySeqItemSupportSuite.scala
+++ src/test/scala/sorm/test/OptionEntitySeqItemSupportSuite.scala
@@ -23,21 +23,20 @@ class OptionEntitySeqItemSupportSuite extends FunSuite with ShouldMatchers {
     val b1 = db.save(B("abc"))
     val b2 = db.save(B("cba"))
 
-    test(dbId + ": saving goes ok"){
-      db.save(A( Seq() ))
-      db.save(A( Seq(Some(b1), None, Some(b2)) ))
-      db.save(A( Seq(None, Some(b2)) ))
-      db.save(A( Seq(None) ))
-    }
+    val a1 = db.save(A( Seq() ))
+    val a2 = db.save(A( Seq(Some(b1), None, Some(b2)) ))
+    val a3 = db.save(A( Seq(None, Some(b2)) ))
+    val a4 = db.save(A( Seq(None) ))
+
     test(dbId + ": empty seq"){
-      db.fetchById[A](1).seq should be === Seq()
+      db.fetchById[A](a1.id).seq should be === Seq()
     }
     test(dbId + ": seq of none"){
-      db.fetchById[A](4).seq should be === Seq(None)
+      db.fetchById[A](a4.id).seq should be === Seq(None)
     }
     test(dbId + ": not empty seqs are correct"){
-      db.fetchById[A](2).seq should be === Seq(Some(b1), None, Some(b2))
-      db.fetchById[A](3).seq should be === Seq(None, Some(b2))
+      db.fetchById[A](a2.id).seq should be === Seq(Some(b1), None, Some(b2))
+      db.fetchById[A](a3.id).seq should be === Seq(None, Some(b2))
     }
   }
 }

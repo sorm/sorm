3574bea7e8daa290c085506f4e7f27957e54ea07
Nikita Volkov <justnika@mail.ru>
2012-11-10 00:16:03 +0400

    cleanup

--- src/test/scala/sorm/test/OptionSupportSuite.scala
+++ src/test/scala/sorm/test/OptionSupportSuite.scala
@@ -6,15 +6,7 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
 import sorm._
-import core._
-import persisted._
-import query._
-import reflection._
-import mappings._
-import jdbc._
 import sext._, embrace._
-
-import samples._
 
 @RunWith(classOf[JUnitRunner])
 class OptionSupportSuite extends FunSuite with ShouldMatchers {

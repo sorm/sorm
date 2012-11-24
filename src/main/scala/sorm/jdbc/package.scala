8e65139834ec9f18efeb0c402f39f298e3abb5eb
Nikita Volkov <justnika@mail.ru>
2012-11-24 05:04:29 +0400

    Statement.simple

--- src/main/scala/sorm/jdbc/package.scala
+++ src/main/scala/sorm/jdbc/package.scala
@@ -50,5 +50,8 @@ object `package` {
   case class Statement
     ( sql: String,
       data: Seq[JdbcValue] = Nil )
-
+  object Statement {
+    def simple ( sql : String, data : Seq[Any] = Nil )
+      = Statement(sql, data.map(JdbcValue(_)))
+  }
 }

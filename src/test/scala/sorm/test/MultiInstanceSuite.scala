03c8ae05b3b478e36117196a18ad9ce009c82280
Nikita Volkov <justnika@mail.ru>
2012-11-24 05:02:56 +0400

    support for overriding dbTypes

--- src/test/scala/sorm/test/MultiInstanceSuite.scala
+++ src/test/scala/sorm/test/MultiInstanceSuite.scala
@@ -2,12 +2,14 @@ package sorm.test
 
 import org.scalatest.{SequentialNestedSuiteExecution, Suite, BeforeAndAfterAll}
 import sorm.Entity
+import sorm.core.DbType
 
 trait MultiInstanceSuite extends Suite with BeforeAndAfterAll with SequentialNestedSuiteExecution {
 
   def entities : Traversable[Entity]
   def poolSizes = 1 :: 6 :: Nil
-  lazy val instancesAndIds = TestingInstances.instances( entities, poolSizes )
+  def dbTypes = DbType.H2 :: DbType.Mysql :: DbType.Hsqldb :: DbType.Postgres :: Nil
+  lazy val instancesAndIds = TestingInstances.instances( entities, poolSizes, dbTypes )
 
   override protected def afterAll() {
     instancesAndIds.unzip._1.foreach(_.close())

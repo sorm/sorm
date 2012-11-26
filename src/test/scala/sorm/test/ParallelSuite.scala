dd5d36a9994a032b41ad55ae734bf7dee9dc6832
Nikita Volkov <justnika@mail.ru>
2012-11-26 22:26:43 +0400

    parallel suite

---
+++ src/test/scala/sorm/test/ParallelSuite.scala
@@ -0,0 +1,28 @@
+package sorm.test
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import sext._, embrace._
+import sorm._
+
+@RunWith(classOf[JUnitRunner])
+class ParallelSuite extends FunSuite with ShouldMatchers with MultiInstanceSuite {
+  import ParallelSuite._
+
+  def entities = Set(Entity[A]())
+  instancesAndIds foreach { case (db, dbId) =>
+    val data = 10 to 200
+    data.par.foreach(v => db.save(A(v)))
+    test(dbId + " - fetching"){
+      db.query[A].order("id").fetch().map(_.a).toSet
+        .should(equal(data.toSet))
+    }
+  }
+
+}
+object ParallelSuite {
+  case class A ( a : Int )
+}

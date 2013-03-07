8f41671c254a47e7d3c6512f770f2b45f9df3790
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-07 05:19:40 +0400

    Driver skeleton

---
+++ core/src/main/scala/sorm/core/Driver.scala
@@ -0,0 +1,35 @@
+package sorm.core
+
+object Driver {
+  trait Driver {
+    /**
+     * 
+     */
+    type ExecutorInstructions
+    /**
+     * Unparsed result returned after executing the instructions, which should
+     * be fed into the parser.
+     */
+    type ExecutorResult
+    val preCompiler : PreCompiler[ExecutorInstructions]
+    val executor : Executor[ExecutorInstructions, ExecutorResult]
+    val parser : Parser[ExecutorResult]
+  }
+
+  // A driver part executed by macros on compile time, which transforms the 
+  // driver-agnostic Action into the driver-specific instructions, thus 
+  // significantly reducing the work to be done at runtime.
+  // In case of RDBMS drivers it makes sense to produce the SQL with it.
+  trait PreCompiler[ Output ] {
+
+  }
+
+  trait Parser[ Input ] {
+
+  }
+
+  trait Executor[ Instructions, Result ] {
+    
+  }
+
+}

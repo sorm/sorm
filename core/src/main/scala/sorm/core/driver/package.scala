f8d3af186054de20dd85d63b1fda96074e8d82bc
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-13 20:32:03 +0400

    Connect things together and update docs

--- core/src/main/scala/sorm/core/driver/package.scala
+++ core/src/main/scala/sorm/core/driver/package.scala
@@ -2,7 +2,8 @@ package sorm.core.driver
 
 trait Driver {
   /**
-   * 
+   * The instructions compiled to the lowest stage. In case of JDBC these should
+   * be Statements.
    */
   type ExecutorInstructions
   /**
@@ -10,12 +11,21 @@ trait Driver {
    * be fed into the parser.
    */
   type ResultSource
-  val preCompiler : PreCompiler[ ExecutorInstructions ]
-  val executor : Executor[ ExecutorInstructions, ResultSource ]
-  val parser : Parser[ ResultSource ]
-
+  /**
+   * A JDBC or an HTTP connection to the database.
+   */
   type Connection
+
+  val preCompiler : PreCompiler[ ExecutorInstructions ]
+  val executor : Executor[ ExecutorInstructions, Connection, ResultSource ]
+  val parser : Parser[ ResultSource ]
   val connector : Connector[ Connection ]
+
+  final def execute[ Result ]( instructions : ExecutorInstructions ) : Result 
+    = connector.withConnection(
+        executor.withResultSource( instructions, _ )( parser.parse )
+      )
+    
 }
 
 // A driver part executed by macros on compile time, which transforms the 
@@ -29,12 +39,17 @@ trait PreCompiler[ Output ] {
 /**
  * @tparam ResultSource In case of JDBC should be a ResultSet
  */
-trait Executor[ Instructions, ResultSource ] {
+trait Executor[ Instructions, Connection, ResultSource ] {
   /**
-   * Delegates the composition with parser to a containing component
+   * Execute the instructions on a connection and feed them to the passed in
+   * function, release all resources afterwards and return the result of that
+   * function.
+   * 
+   * In case of JDBC this method should handle the proper closing of 
+   * `ResultSet`.
    */
   def withResultSource[ Result ]
-    ( instructions : Instructions )
+    ( instructions : Instructions, connection : Connection )
     ( f : ResultSource => Result )
     : Result
 }
@@ -46,6 +61,9 @@ trait Parser[ Input ] {
   def parse[ Output ]( input : Input ) : Output
 }
 
+/**
+ * A manager of database connections. In case of JDBC a connection pooler.
+ */
 trait Connector[ Connection ] {
   def withConnection[ Result ]( f : Connection => Result ) : Result
 }

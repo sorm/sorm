efe70b2d510cd1967facf6f1bafaac7a5cdc0452
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-15 20:38:29 +0400

    Add Macros and equals impl

--- core/src/main/scala/sorm/core/api/where/package.scala
+++ core/src/main/scala/sorm/core/api/where/package.scala
@@ -2,25 +2,33 @@ package sorm.core.api.where
 
 import sorm.core.{instructions => Instructions}
 import sorm.core.subRef._
+import language.experimental.macros
 
 /**
  * `.where( _.exists( _.genres, _.equals( _.name, "metal" ) ) )
  */
 class WhereComposer
-  [ Context, Input ]
-  ( val instructions : Instructions.Filters[ Context, Input ],
+  [ Entity, Input ]
+  ( val instructions : Instructions.Filters[ Entity, Input ],
     val input : Input )
   {
 
+    def equals
+      [ Value ]
+      ( ref : Entity => Value,
+        value : Value )
+      : WhereComposer[ Entity, (Value, Input) ]
+      = macro Macros.equals[ Entity, Value, Input ]
+
     def equalsImpl
       [ Value ]
-      ( ref : SubRef[ Context, Value ],
+      ( ref : SubRef[ Entity, Value ],
         value : Value )
-      : WhereComposer[ Context, (Value, Input) ]
+      : WhereComposer[ Entity, (Value, Input) ]
       = {
         val newInstructions = Instructions.Comparison(
           ref,
-          Instructions.OutputValue[ Context, Value ](),
+          Instructions.OutputValue[ Entity, Value ](),
           Instructions.Equal,
           false,
           instructions
@@ -33,15 +41,28 @@ class WhereComposer
       [ Value <: Traversable[ ValueItem ],
         ValueItem,
         SubInput ]
-      ( ref : SubRef[ Context, Value ], 
+      ( ref : SubRef[ Entity, Value ], 
         where : WhereComposer[ ValueItem, Unit ] => 
                 WhereComposer[ ValueItem, SubInput ] )
-      : WhereComposer[ Context, (SubInput, Input) ]
+      : WhereComposer[ Entity, (SubInput, Input) ]
       = ???
 
   }
 
 private object Macros {
+
+  import reflect.runtime.universe._
+  import reflect.macros.Context
+
+  def equals
+    [ Entity : c.WeakTypeTag, 
+      Value : c.WeakTypeTag,
+      Input ]
+    ( c : Context )
+    ( ref : c.Expr[ Entity => Value ],
+      value : c.Expr[ Value ] )
+    : c.Expr[ WhereComposer[ Entity, (Value, Input) ] ]
+    = ???
 
 }
 

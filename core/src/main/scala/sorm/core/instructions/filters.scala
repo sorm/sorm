f0c870549fddd71f7f9132989c0d23256281b753
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-15 16:05:15 +0400

    Extend filters

--- core/src/main/scala/sorm/core/instructions/filters.scala
+++ core/src/main/scala/sorm/core/instructions/filters.scala
@@ -23,9 +23,31 @@ case class And
     tail : Filters[ Entity, TailInput] )
   extends Filters[ Entity, (Input1, Input2, TailInput) ]
 
-case class Equals
+case class Comparison
   [ Entity, ValueOutput, ValueInput, TailInput ]
   ( subRef : SubRef[ Entity, ValueOutput ],
     value : Select[ Entity, ValueInput, ValueOutput ],
+    operator : Operator,
+    negative : Boolean,
     tail : Filters[ Entity, TailInput ] )
   extends Filters[ Entity, (ValueInput, TailInput) ]
+
+
+sealed trait Operator
+case object Equal extends Operator
+case object Larger extends Operator
+case object Smaller extends Operator
+case object Like extends Operator
+case object Regex extends Operator
+case object In extends Operator
+case object Contains extends Operator
+/**
+ * Makes part of a collection
+ */
+case object Constitutes extends Operator
+/**
+ * Includes a collection
+ */
+case object Includes extends Operator
+case object HasSize extends Operator
+

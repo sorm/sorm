0c9d4ad8a174b7254b5786a66a0e69a0efac7ca6
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-18 15:03:35 +0400

    OrderSupport

--- core/src/main/scala/sorm/core/api/select/package.scala
+++ core/src/main/scala/sorm/core/api/select/package.scala
@@ -8,7 +8,7 @@ import sorm.core.subRef._
  * @tparam Driver The driver, for which the instruction is to be composed.
  * Needed for statically checking Driver's support for type-specific operations.
  * @tparam Entity The context entity. Required for `_.field` references.
- * In case of SQL this also defines the main table, to which others are to be 
+ * In case of SQL this also defines the main table, to which others are to be
  * joined.
  * @tparam Input The composed type of all input values for the generated 
  * instructions.
@@ -27,10 +27,20 @@ class SelectComposer
       : SelectComposer[ Driver, Entity, (FiltersInput, Input), Output ]
       = ???
 
+    /**
+     *
+     * @param ref A reference to the field, by which to order.
+     * @param reverse Whether the ordering should be descending.
+     * @param support An implicit evidence of a driver support for ordering
+     *                by values of presented Value type.
+     * @tparam Value Type of referred value.
+     * @return New SelectComposer.
+     */
     def order
       [ Value ]
       ( ref : SubRef[ Entity, Value ],
         reverse : Boolean )
+      ( implicit support : OrderSupport[ Driver, Value ] )
       : SelectComposer[ Driver, Entity, Input, Output ]
       = new SelectComposer(
           driver,
@@ -44,18 +54,24 @@ class SelectComposer
 
   }
 
+/**
+ * Implicit instances of this type denote the driver's support for ordering by
+ * specific type.
+ */
+trait OrderSupport[ Driver, -Value ]
+
 trait InstanceSelectSupport[ Driver <: DriverSelectSupport ] {
 
   val driver : Driver
 
   def select
-    [ Entity ] 
+    [ Entity ]
     : SelectComposer[ Driver, Entity, Unit, Seq[ Entity ] ]
     = new SelectComposer( driver, Instructions.OutputEntity[ Entity ](), Unit )
 
 }
 
 trait DriverSelectSupport {
-  
+
 }
 

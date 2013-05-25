fb432e97178c6c6bff3c38c30d33674a084eb576
Eugene Burmako <xeno.by@gmail.com>
2013-05-25 20:02:27 +0200

    implements Macros.equals

--- core/src/main/scala/sorm/core/api/where/package.scala
+++ core/src/main/scala/sorm/core/api/where/package.scala
@@ -131,7 +131,53 @@ private object Macros {
     ( ref : c.Expr[ Entity => Value ],
       value : c.Expr[ Value ] )
     : c.Expr[ WhereComposition[ Driver, Entity, (Value, Input) ] ]
-    = ???
+    = {
+
+    import c.universe._
+
+    val Entity = TypeTree(weakTypeOf[Entity])
+    val Value = TypeTree(weakTypeOf[Value])
+    def ru = treeBuild.mkRuntimeUniverseRef
+    def reifyType(tpe: Type) = c.reifyType(ru, EmptyTree, tpe, concrete = true)
+
+    object PropertyPath {
+      def unapply(tree: Tree): Option[List[RefTree]] = tree match {
+        case tree @ Ident(name) if name.isTermName => Some(List(tree))
+        case tree @ Select(prefix, name) if name.isTermName => unapply(prefix).map(_ :+ tree)
+        case _ => None
+      }
+    }
+
+    val reifiedSymbols = ref.tree match {
+      case Function(List(ValDef(_, paramName,_, _)), PropertyPath(target :: props)) if target.name == paramName =>
+        def loop(tpe: Type, props: List[RefTree], syms: List[Symbol]): List[Symbol] = props match {
+          case prop :: others =>
+            val sym = tpe.members.collect{ case m: MethodSymbol if m.name == prop.name && m.isGetter => m }.headOption
+            sym match {
+              case Some(sym) => loop(sym.returnType, others, syms :+ sym)
+              // FIXME: any additional validation? does the owner have to be a case class? a top-level case class?
+              case None => c.abort(prop.pos, s"only getters are supported in equals clauses")
+            }
+          case Nil => syms
+        }
+        val symbols = loop(Entity.tpe, props, Nil)
+        symbols.map(symbol => {
+          val reifiedOwner = Select(Select(reifyType(symbol.owner.asType.toType), newTermName("tpe")), newTermName("typeSymbol"))
+          val selectTerm = Select(Select(ru, newTermName("build")), newTermName("selectTerm"))
+          Apply(selectTerm, List(reifiedOwner, Literal(Constant(symbol.name.toString))))
+        })
+      case _ =>
+        println(showRaw(ref.tree))
+        c.abort(c.enclosingPosition, s"only property paths are supported in equals clauses")
+    }
+
+    val SubRef = Select(Select(Select(Ident(newTermName("sorm")), newTermName("core")), newTermName("subRef")), newTermName("SubRef"))
+    val contextType = Select(reifyType(weakTypeOf[Entity]), newTermName("tpe"))
+    val subFieldSymbols = Apply(Ident(newTermName("List")), reifiedSymbols)
+    val subRef = Apply(TypeApply(SubRef, List(Entity, Value)), List(contextType, subFieldSymbols))
+
+    c.Expr(Apply(Select(c.prefix.tree, newTermName("equals")), List(subRef, value.tree)))
+  }
 
 }
 

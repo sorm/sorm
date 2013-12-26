6be2a9fa4f937acc34a387a9d51cf44d6ae2a7d3
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-12-26 05:31:12 +0400

    VerifyError scala issue reproduction

--- core/src/test/scala/sorm/core/SetupTest.scala
+++ core/src/test/scala/sorm/core/SetupTest.scala
@@ -9,12 +9,21 @@ class SetupTest extends FunSuite {
 
   case class A(a: Int, b: String)
 
+// Running the following fails with
+// java.lang.VerifyError: (class: sorm/core/SetupTest$$anonfun$1$$anon$1, method: <init> signature: (Lsorm/core/SetupTest$$anonfun$1;)V) Expecting to find object/array on stack
   test("Member is reachable") {
-    val member = Setup.Member(PersistedMixiner.derive[A], Set())
-    val setup = new Setup(member :: HList.Nil) {}
+    val setup = new Setup(Setup.Member(PersistedMixiner.derive[A], Set()) :: HList.Nil) {}
     import setup._
 
     implicitly[Setup.Member[A]]
   }
+// However, if we simply declare `member` as a new variable, as in the following, it will run fine.
+//  test("Member is reachable") {
+//    val member = Setup.Member(PersistedMixiner.derive[A], Set())
+//    val setup = new Setup(member :: HList.Nil) {}
+//    import setup._
+//
+//    implicitly[Setup.Member[A]]
+//  }
 
 }

1ec194d61355260567ff2342a19469c375df0614
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-01-24 13:44:00 +0400

    Compiler function composition

--- relational/src/main/scala/sorm/relational/sql/Compiler.scala
+++ relational/src/main/scala/sorm/relational/sql/Compiler.scala
@@ -1,6 +1,6 @@
 package sorm.relational.sql
 
-import sorm._, core._
+import sorm._, core._, relational._
 
 /**
  * A general compiler interface. 
@@ -8,113 +8,153 @@ import sorm._, core._
  */
 // NOTE: Since all the compilation happens to dynamic types, 
 // typeclasses wouldn't really have helped here.
-trait Compiler extends Renderer with Arranger
+trait Compiler {
+  import sql.{templates => t}
 
-protected sealed trait Renderer {
-  import templates._
+  case class Output( template: String, f: F )
+  type F = Any => Seq[Value]
 
-  def render( a: Statement ): String = a match {
-    case a: Statement.Insert => render(a)
-    case a: Statement.Update => render(a)
-    case a: Statement.Delete => render(a)
-    case a: Statement.Select => render(a)
-    case a: Statement.Union => render(a)
+  protected val voidF: F = _ => Nil
+  protected def pairF( left: Output, right: Output ): F = { case (a, b) => left.f(a) ++ right.f(b) }
+  protected def seqF( fs: Seq[F] ): F = { case a: Seq[Any] =>
+    val noF = bug("Not enough functions")
+    val noValues = bug("Not enough values")
+    fs.zipAll(a, noF, noValues).flatMap{ case (a, b) => a(b) }
   }
-  def render( a: Statement.Insert ) = ??? : String
-  def render( a: Statement.Update ) = ??? : String
-  def render( a: Statement.Delete ) = ??? : String
-  def render( a: Statement.Select ): String = {
-    val distinct = if( a.distinct ) " DISTINCT" else ""
-    val from = render(a.from)
-    val where = a.where.map(render).map("WHERE " +)
+
+  def compile( a: t.Statement ): Output = a match {
+    case a: t.Statement.Insert => compile(a)
+    case a: t.Statement.Update => compile(a)
+    case a: t.Statement.Delete => compile(a)
+    case a: t.Statement.Select => compile(a)
+    case a: t.Statement.Union => compile(a)
+  }
+  def compile( a: t.Statement.Insert ) = ??? : Output
+  def compile( a: t.Statement.Update ) = ??? : Output
+  def compile( a: t.Statement.Delete ) = ??? : Output
+  def compile( a: t.Statement.Select ): Output = {
+    val from = compile(a.from)
+    val where = a.where.map(compile)
+    val template = {
+      val distinct = if( a.distinct ) " DISTINCT" else ""
+      val whereT = where.map("WHERE " +)
+      ???
+    }
     ???
-    s"SELECT$distinct"
   }
-  def render( a: Statement.Union ): String = {
-    s"(${render(a.left)}) UNION (${render(a.right)})"
+  def compile( a: t.Statement.Union ): Output = {
+    val left = compile(a.left)
+    val right = compile(a.right)
+    val template = s"(${left.template}) UNION (${compile(a.right)})"
+    val f = pairF(left, right)
+    Output(template, f)
   }
-  def render( a: What ): String = {
-    val exprs = a.head +: a.tail
-    exprs.map(render).mkString(", ")
+  def compile( a: t.What ): Output = {
+    val exprs = (a.head +: a.tail.toStream).map(compile)
+    val template = exprs.map(_.template).mkString(", ")
+    val f: F = seqF(exprs.map(_.f))
+    Output(template, f)
   }
-  def render( a: WhatExpr ): String = a match {
-    case a: WhatExpr.Ref => render(a)
-    case a: WhatExpr.AllColumns => render(a)
-    case a: WhatExpr.Count => render(a)
+  def compile( a: t.WhatExpr ): Output = a match {
+    case a: t.WhatExpr.Ref => compile(a)
+    case a: t.WhatExpr.AllColumns => compile(a)
+    case a: t.WhatExpr.Count => compile(a)
   }
-  def render( a: WhatExpr.Ref ): String = render(a.ref)
-  def render( a: WhatExpr.AllColumns ): String = {
-    a.table.map(render).map(_ + ".").getOrElse("") + "*"
+  def compile( a: t.WhatExpr.Ref ): Output = compile(a.ref)
+  def compile( a: t.WhatExpr.AllColumns ): Output = {
+    val table = a.table.map(compile)
+    val template = table.map(_.template).map(_ + ".").getOrElse("") + "*"
+    val f = table.map(_.f).getOrElse(voidF)
+    Output(template, f)
   }
-  def render( a: WhatExpr.Count ): String = ???
-  def render( a: From ): String = {
-    val expr = render(a.expr)
-    val as = a.as.map(render).map(" AS " +).getOrElse("")
-    val joins = a.joins.view.map(render).map(" " +).mkString
-    "FROM " + expr + as + joins
+  def compile( a: t.WhatExpr.Count ): Output = ???
+  def compile( a: t.From ): Output = {
+    val expr = compile(a.expr)
+    val as = a.as.map(compile)
+    val joins = a.joins.toStream.map(compile)
+    val template = {
+      val exprT = expr.template
+      val asT = as.map(_.template).map(" AS " +).getOrElse("")
+      val joinsT = joins.map(_.template).map(" " +).mkString
+      "FROM " + exprT + asT + joinsT
+    }
+    val f: F = { case (a, b, c: Seq[Any]) =>
+      val exprV = expr.f(a)
+      val asV = as.map(_.f(b)).getOrElse(Nil)
+      val joinsV = seqF(joins.map(_.f))(c)
+      exprV ++ asV ++ joinsV
+    }
+    Output(template, f)
   }
-  def render( a: FromExpr ): String = a match {
-    case a: FromExpr.Table => render(a)
+  def compile( a: t.FromExpr ): Output = a match {
+    case a: t.FromExpr.Table => compile(a)
   }
-  def render( a: FromExpr.Table ): String = render(a.name)
-  def render( a: Join ): String = ???
-  def render( a: Condition ): String = a match {
-    case a: Condition.Fork => render(a)
-    case a: Condition.Comparison => render(a)
-    case a: Condition.IsNull => render(a)
+  def compile( a: t.FromExpr.Table ): Output = compile(a.name)
+  def compile( a: t.Join ): Output = ???
+  def compile( a: t.Condition ): Output = a match {
+    case a: t.Condition.Fork => compile(a)
+    case a: t.Condition.Comparison => compile(a)
+    case a: t.Condition.IsNull => compile(a)
   }
-  def render( a: Condition.Fork ): String = {
-    val operator = if( a.or ) "OR" else "AND"
-    "(" + render(a.left) + " " + operator + " " + render(a.right) + ")"
+  def compile( a: t.Condition.Fork ): Output = {
+     val left = compile(a.left)
+     val right = compile(a.right)
+     val template = {
+      a.or match {
+        case true => s"(${left.template} OR ${right.template})"
+        case false => s"${left.template} AND ${right.template}"
+      }
+     }
+     Output(template, pairF(left, right))
   }
-  def render( a: Condition.Comparison ): String = {
-    val operator = render(a.operator, a.negative)
-    render(a.left) + " " + operator + " " + render(a.right)
+  def compile( a: t.Condition.Comparison ): Output = {
+    val operator = compile(a.operator, a.negative)
+    val left = compile(a.left)
+    val right = compile(a.right)
+    val template = left.template + " " + operator.template + " " + right.template
+    val f: F = { case (a, b) => left.f(a) ++ right.f(b) }
+    Output(template, f)
   }
-  def render( a: Condition.IsNull ): String = {
-    render(a.expr) + " IS" + (if( a.negative ) " NOT" else "") + " NULL"
+  def compile( a: t.Condition.IsNull ): Output = {
+    val expr = compile(a.expr)
+    val template = expr.template + " IS" + (if( a.negative ) " NOT" else "") + " NULL"
+    val f = expr.f
+    Output(template, f)
   }
-  def render( a: (Operator, Boolean) ): String = a match {
-    case (Operator.Equal, false) => "="
-    case (Operator.Equal, true) => "<>"
-    case (Operator.Larger, false) => ">"
-    case (Operator.Larger, true) => "<="
-    case (Operator.Smaller, false) => "<"
-    case (Operator.Smaller, true) => ">="
-    case (Operator.Like, false) => "LIKE"
-    case (Operator.Like, true) => "NOT LIKE"
-    case (Operator.Regexp, false) => "REGEXP"
-    case (Operator.Regexp, true) => "NOT REGEXP"
-    case (Operator.In, false) => "IN"
-    case (Operator.In, true) => "NOT IN"
+  def compile( a: (t.Operator, Boolean) ): Output = a match {
+    case (t.Operator.Equal, false) => Output("=", voidF)
+    case (t.Operator.Equal, true) => Output("<>", voidF)
+    case (t.Operator.Larger, false) => Output(">", voidF)
+    case (t.Operator.Larger, true) => Output("<=", voidF)
+    case (t.Operator.Smaller, false) => Output("<", voidF)
+    case (t.Operator.Smaller, true) => Output(">=", voidF)
+    case (t.Operator.Like, false) => Output("LIKE", voidF)
+    case (t.Operator.Like, true) => Output("NOT LIKE", voidF)
+    case (t.Operator.Regexp, false) => Output("REGEXP", voidF)
+    case (t.Operator.Regexp, true) => Output("NOT REGEXP", voidF)
+    case (t.Operator.In, false) => Output("IN", voidF)
+    case (t.Operator.In, true) => Output("NOT IN", voidF)
   }
-  def render( a: Expr ): String = a match {
-    case a: Expr.Placeholder => render(a)
-    case a: Expr.Constant => render(a)
-    case a: Expr.Select => render(a)
-    case a: Expr.Ref => render(a)
+  def compile( a: t.Expr ): Output = a match {
+    case a: t.Expr.Placeholder => compile(a)
+    case a: t.Expr.Constant => compile(a)
+    case a: t.Expr.Select => compile(a)
+    case a: t.Expr.Ref => compile(a)
   }
-  def render( a: Expr.Placeholder ): String = render(a.placeholder)
-  def render( a: Expr.Constant ) = ???
-  def render( a: Expr.Select ) = ???
-  def render( a: Expr.Ref ): String = render(a.ref)
-  def render( a: Placeholder ) = "?"
-  def render( a: Ref ): String = {
-    val context = a.context.map(render).map(_ + ".").getOrElse("")
-    val symbol = render(a.symbol) 
-    context + symbol
+  def compile( a: t.Expr.Placeholder ): Output = compile(a.placeholder)
+  def compile( a: t.Expr.Constant ) = ???
+  def compile( a: t.Expr.Select ) = ???
+  def compile( a: t.Expr.Ref ): Output = compile(a.ref)
+  def compile( a: t.Placeholder ) = Output("?", voidF)
+  def compile( a: t.Ref ): Output = {
+    val context = a.context.map(compile)
+    val symbol = compile(a.symbol)
+    val template = context.map(_.template + ".").getOrElse("") + symbol.template
+    Output(template, voidF)
   }
-  def render( a: Identifier ) = "\"" + a.value + "\""
-
-}
-
-protected sealed trait Arranger {
-  import values._
-
-  def arrange( a: Statement ): Seq[Any] = a match {
-    case a: Statement.Select => arrange(a)
+  def compile( a: t.Identifier ) = {
+    val template = "\"" + a.value + "\""
+    Output(template, voidF)
   }
-  def arrange( a: Statement.Select ) = ??? : Seq[Any]
-  def arrange( a: Statement.Insert ) = ??? : Seq[Any]
 
 }

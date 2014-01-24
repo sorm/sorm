e85df5dc92488167dd1305c75d299f37ce5e15bc
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-01-24 18:30:49 +0400

    util IterableLike with zipAllLazy extension

--- core/src/main/scala/sorm/core/util.scala
+++ core/src/main/scala/sorm/core/util.scala
@@ -1,4 +1,8 @@
-package sorm.core; package object util {
+package sorm.core
+
+import collection._, generic._
+
+package object util {
 
   def memo [ a, z ] ( f : a => z ) = {
      // a WeakHashMap will release cache members if memory tightens
@@ -49,6 +53,24 @@ package sorm.core; package object util {
     def generic(t: Type, i: Int) = t.asInstanceOf[TypeRef].args(i)
     def name(s: Symbol) = s.name.decoded.trim
     def properties(t: Type) = t.members.toStream.filter(_.isTerm).filter(!_.isMethod).reverse
+  }
+
+  implicit class IterableLikeUtil[+A, +Repr]( val base: IterableLike[A, Repr] ) extends AnyVal {
+    private def repr: Repr = base.asInstanceOf[Repr]
+    def zipAllLazy[B, A1 >: A, That](that: GenIterable[B], thisElem: =>A1, thatElem: =>B)(implicit bf: CanBuildFrom[Repr, (A1, B), That]): That = {
+      val b = bf(repr)
+      val these = base.iterator
+      val those = that.iterator
+      while (these.hasNext && those.hasNext)
+        b += ((these.next, those.next))
+      while (these.hasNext) {
+        b += ((these.next, thatElem))
+      }
+      while (those.hasNext) {
+        b += ((thisElem, those.next))
+      }
+      b.result
+    }
   }
 
 }

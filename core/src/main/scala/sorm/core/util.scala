a248906cb1401c2361287fdd165212a695b29a26
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-01-31 15:06:15 +0400

    isTuple correction

--- core/src/main/scala/sorm/core/util.scala
+++ core/src/main/scala/sorm/core/util.scala
@@ -46,29 +46,7 @@ package object util {
 
   object reflection {
     import reflect.runtime.universe._
-    def isTuple(t: Type) =
-      t <:< typeOf[Tuple1[_]] ||
-      t <:< typeOf[Tuple2[_, _]] ||
-      t <:< typeOf[Tuple3[_, _, _]] ||
-      t <:< typeOf[Tuple4[_, _, _, _]] ||
-      t <:< typeOf[Tuple5[_, _, _, _, _]] ||
-      t <:< typeOf[Tuple6[_, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple7[_, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple8[_, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple9[_, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple10[_, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple11[_, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple12[_, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple13[_, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple14[_, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple15[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple16[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple17[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple18[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple19[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple20[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple21[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]] ||
-      t <:< typeOf[Tuple22[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]]
+    def isTuple(t: Type) = t.typeSymbol.fullName.startsWith("scala.Tuple")
     def generic(t: Type, i: Int) = t.asInstanceOf[TypeRef].args(i)
     def name(s: Symbol) = s.name.decoded.trim
     def properties(t: Type) = t.members.toStream.filter(_.isTerm).filter(!_.isMethod).reverse
